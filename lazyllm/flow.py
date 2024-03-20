b2a3e795543808e49574ee870bcf351603bba2f8
wangzhihong <wangzhihong@sensetime.com>
2024-03-20 13:40:00 +0800

    add repr for module and agent example (dialog)

--- lazyllm/flow.py
+++ lazyllm/flow.py
@@ -48,10 +48,10 @@ class LazyLLMFlowsBase(FlowBase, metaclass=LazyLLMRegisterMetaClass):
         def __call__(self, *args, **kw): return self.f(*args, **kw)
         def __repr__(self): return repr(self.f)
     
-    def __init__(self, *args, post_action=None, **kw):
+    def __init__(self, *args, post_action=None, return_input=False, **kw):
         assert len(args) == 0 or len(kw) == 0
         if len(args) > 0 and isinstance(args[0], (tuple, list)):
-            assert len(args) == 1
+            assert len(args) == 1, f'{self.__class__} init failed'
             args = args[0]
         args = list(args)
         for k, v in kw.items():
@@ -67,11 +67,14 @@ class LazyLLMFlowsBase(FlowBase, metaclass=LazyLLMRegisterMetaClass):
             args.append(v)
         super(__class__, self).__init__(*args)
         self.post_action = post_action() if isinstance(post_action, type) else post_action
+        self.return_input = return_input
 
     def __call__(self, args=package()):
         output = self._run(args)
         if self.post_action is not None:
             self.post_action(*output) if isinstance(output, package) else self.post_action(output) 
+        if self.return_input:
+            output = package(args, output)
         return output
     
     def _run(self, *args, **kw):
@@ -82,7 +85,7 @@ class LazyLLMFlowsBase(FlowBase, metaclass=LazyLLMRegisterMetaClass):
         def _exchange(item):
             item._args = [a.get_from(self) if isinstance(a, type(root)) else a for a in item._args]
         self.for_each(lambda x: isinstance(x, bind), _exchange)
-        return self, self(*args, **kw)
+        return self, self(package(*args), **kw)
 
     def __repr__(self):
         representation = '' if self._flow_name is None else (self._flow_name + ' ')
@@ -124,8 +127,8 @@ class Pipeline(LazyLLMFlowsBase):
 
 
 class NamedPipeline(Pipeline):
-    def __init__(self, *, post_action=None, **kw):
-        super().__init__(post_action=post_action, **kw)
+    def __init__(self, *, post_action=None, return_input=False, **kw):
+        super().__init__(post_action=post_action, return_input=return_input, **kw)
 
 
 _barr = threading.local()
@@ -152,8 +155,8 @@ class Parallel(LazyLLMFlowsBase):
 
 
 class NamedParallel(Parallel):
-    def __init__(self, *, post_action=None, **kw):
-        super().__init__(post_action=post_action, **kw)
+    def __init__(self, *, post_action=None, return_input=False, **kw):
+        super().__init__(post_action=post_action, return_input=return_input, **kw)
 
 
 # parallel in dataflow, serial in executing. dataflow is the same as parallel, while
@@ -196,30 +199,49 @@ class Warp(LazyLLMFlowsBase):
 class Switch(LazyLLMFlowsBase):
     # Switch({cond1: M1, cond2: M2, ..., condN: MN})
     # Switch(cond1, M1, cond2, M2, ..., condN, MN)
-    def __init__(self, *args, post_action=None, **kw):
+    def __init__(self, *args, post_action=None, return_input=False, **kw):
         if len(args) == 1 and isinstance(args[0], dict):
-            self.keys, items = list(args[0].keys()), list(args[0].values())
+            self.conds, items = list(args[0].keys()), list(args[0].values())
         else:
-            self.keys, items = args[0::2], args[1::2]
-        super().__init__(*items, post_action=post_action, **kw)
+            self.conds, items = args[0::2], args[1::2]
+        items = {repr(k)+':': v for k, v in zip(self.conds, items)}
+        super().__init__(**items, post_action=post_action, return_input=return_input, **kw)
 
     def _run(self, input=package()):
-        assert isinstance(input, package) and len(input) == 2
-        exp, input = input
+        exp = input
+        if isinstance(input, package) and len(input) == 2 and not callable(self.conds[0]):
+            exp, input = input
         for idx, cond in enumerate(self.conds):
-            if (callable(cond) and cond(exp) is True) or exp == cond or cond == 'default':
+            if (callable(cond) and invoke(cond, exp) is True) or (exp == cond) or cond == 'default':
                 return invoke(self.items[idx], input)
 
-#  in -> module1 -> ... -> moduleN -> exp, out -> out
-#   ⬆------------------------------------|
-class Loop(LazyLLMFlowsBase):
-    def __init__(self, item, cond, post_action=None):
-        super().__init__(item, post_action=post_action)
-        self.cond = cond
+
+# result = cond(input) ? tpath(input) : fpath(input)
+class IFS(LazyLLMFlowsBase):
+    def __init__(self, cond, tpath, fpath, post_action=None, return_input=False):
+        super().__init__(cond, tpath, fpath, post_action=post_action, return_input=return_input)
 
     def _run(self, input=package()):
+        cond, tpath, fpath = self.items
+        return invoke(tpath, input) if invoke(cond, input) else invoke(fpath, input) 
+
+
+#  in(out) -> module1 -> ... -> moduleN -> exp, out -> out
+#      ⬆----------------------------------------|
+class Loop(LazyLLMFlowsBase):
+    def __init__(self, *item, stop_condition=None, count=None, post_action=None, return_input=False):
+        super().__init__(*item, post_action=post_action, return_input=return_input)
+        assert (callable(stop_condition) and count is None) or (
+            stop_condition is None and isinstance(count, int))
+        self.cond = stop_condition
+        self.count = count
+
+    def _run(self, input=package()):
+        cnt = 0
         while True:
-            exp, input = invoke(self.items[0], input)
-            if (callable(self.cond) and self.cond(exp) is True) or self.cond == exp:
+            for item in self.items:
+                input = invoke(item, input)
+            cnt += 1
+            if (callable(self.cond) and self.cond(input)) or (self.count is not None and cnt >= self.count):
                 break
         return input
