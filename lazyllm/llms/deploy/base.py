b8c90699ce35a6358cd3ef8d59c2391387a20694
wangzhihong <wangzhihong@sensetime.com>
2024-04-03 14:03:15 +0800

    Refactor of launchers

--- lazyllm/llms/deploy/base.py
+++ lazyllm/llms/deploy/base.py
@@ -3,7 +3,7 @@ import time
 import requests
 from ..core import LLMBase
 import lazyllm
-from lazyllm import launchers, flows, LazyLLMCMD, timeout
+from lazyllm import launchers, flows
 import random
 
 
@@ -42,39 +42,11 @@ def verify_fastapi_func(job):
         line = job.queue.get()
         if line.startswith('ERROR:'):
             print("Capture error message: ", line, "\n\n")
-            return False, line
+            return False
         elif 'Uvicorn running on' in line:
             print("Capture startup message:   ",line)
             break
         if job.status == lazyllm.launchers.status.Failed:
-            return False, 'Service Startup Failed.'
-    return True, line
-
-
-def restart_service(job, port, headers, data, build_cmd=None, verify_func=verify_fastapi_func, count=0):
-    assert count < 5, 'The service failed to restart 5 times.'
-    
-    if lazyllm.mode == lazyllm.Mode.Display:
-        return f'http://{job.name}:{port}/generate'
-    status = launchers.status
-    with timeout(3600, msg='Launch failed: No computing resources are available.'):
-        while job.status in (status.TBSubmitted, status.InQueue, status.Pending):
-            time.sleep(2)
-    
-    assert job.status == lazyllm.launchers.status.Running, 'Job failed'
-    url = f'http://{job.get_jobip()}:{port}/generate'
-
-    with timeout(240, msg='Service encountered an unknown exception.'):
-        verify_res, verify_str = verify_func(job)
-        data = 'Hello world~' if not data else data
-        if verify_res:
-            _ = requests.post(url, headers=headers, data=json.dumps(data))
-            job.return_value = url
-            return url
-        elif build_cmd:
-            cmd, port = build_cmd()
-            job.cmd = LazyLLMCMD(cmd=cmd)
-            job.restart()
-            return restart_service(job, port, headers, data, build_cmd, verify_func, count+1)
-        else:
-            raise verify_str
+            print("Service Startup Failed.")
+            return False
+    return True
--- lazyllm/llms/deploy/relay/base.py
+++ lazyllm/llms/deploy/relay/base.py
@@ -1,12 +1,10 @@
 import os
 import random
 import base64
-
-import lazyllm
-from lazyllm import launchers, LazyLLMCMD, bind, _0
-from ..base import LazyLLMDeployBase, restart_service
-
+from lazyllm import launchers, LazyLLMCMD
+from ..base import LazyLLMDeployBase, verify_fastapi_func
 from lazyllm.thirdparty import cloudpickle
+
 
 def dump_func(f, old_value=None):
     f = old_value if f is None else f
@@ -32,26 +30,19 @@ class RelayServer(LazyLLMDeployBase):
         folder_path = os.path.dirname(os.path.abspath(__file__))
         run_file_path = os.path.join(folder_path, 'server.py')
 
-        def build_cmd():
-            port = self.port if self.port else random.randint(30000, 40000)
-            cmd = f'python {run_file_path} --open_port={port} '
-            cmd += f'--function="{self.func}" '
+        def impl():
+            self.port = self.port if self.port else random.randint(30000, 40000)
+            cmd = f'python {run_file_path} --open_port={self.port} --function="{self.func}" '
             if self.pre:
                 cmd += f'--before_function="{self.pre}" '
             if self.post:
                 cmd += f'--after_function="{self.post}" '
-            return cmd, port
+            return cmd
 
-        cmd, port = build_cmd()
-        func = build_cmd if not self.port else None
-        return LazyLLMCMD(cmd=cmd,
-                          post_function=bind(
-                              restart_service,
-                              _0,
-                              port,
-                              self.default_headers,
-                              self.message_formate,
-                              func),
-                          no_displays=['function', 'before_function', 'after_function']
-                         )
-    
+        return LazyLLMCMD(cmd=impl, return_value=self.geturl, checkf=verify_fastapi_func,
+                          no_displays=['function', 'before_function', 'after_function'])
+
+    def geturl(self, job=None):
+        if job is None:
+            job = self.job
+        return f'http://{job.get_jobip()}:{self.port}/generate'
