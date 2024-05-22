953eea04cb7e3add7b2c99da8099772c8b0d1d1e
liuzequn <liuzequn@sensetime.com>
2024-05-22 10:40:30 +0800

    add test

--- tests/test_thirdparty.py
+++ tests/test_thirdparty.py
@@ -1,9 +1,26 @@
+import sys
 import pytest
 
 class TestFn_Thirdparty(object):
     
-    def test_import(self):
-        with pytest.raises(ModuleNotFoundError) as e:
-            import llamaindex
-            
-        
+    def test_import(self, monkeypatch):
+        def check_installed():
+            try:
+                import llama_index
+                return True
+            except ImportError:
+                return False
+        monkeypatch.delitem(sys.modules, "llama_index", raising=False)
+        assert check_installed() == False
+
+    def test_lazy_import(self, monkeypatch):
+        def check_lazy_import(llama_index):
+            try:
+                llama_index.a
+                return True
+            except ImportError:
+                return False
+        monkeypatch.delitem(sys.modules, "llama_index", raising=False)
+        from lazyllm.thirdparty import llama_index
+        assert llama_index is not None
+        assert check_lazy_import(llama_index) == False
