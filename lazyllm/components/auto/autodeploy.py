e57d6ebe149fd50daf352df527ca700f00425bb1
wangzhihong <wangzhihong@sensetime.com>
2024-06-12 18:10:58 +0800

    fix pipeline.bind bug

--- lazyllm/components/auto/autodeploy.py
+++ lazyllm/components/auto/autodeploy.py
@@ -30,4 +30,3 @@ class AutoDeploy(LazyLLMDeployBase):
                     kw[value] = getattr(c, key)
             return deploy_cls(trust_remote_code=trust_remote_code, launcher=launcher, stream=stream, **kw)
         raise RuntimeError(f'No valid framework found, candidates are {[c.framework.lower() for c in candidates]}')
-
