8b160d2fcf17d0c8cc61db6fb140bda2933ee379
wangjian052163 <wangjian052163@sina.com>
2024-06-13 20:01:48 +0800

    modify onlineChatModule prompter setting (#3)
    Co-authored-by: wangjian <wangjian2@sensetime.com>

--- lazyllm/module/onlineChatModule/doubaoModule.py
+++ lazyllm/module/onlineChatModule/doubaoModule.py
@@ -8,17 +8,18 @@ class DoubaoModule(OnlineChatModuleBase):
     def __init__(self,
                  model: str,
                  base_url: str = "https://ark.cn-beijing.volces.com/api/v3",
-                 system_prompt: str = "You are a helpful assistant.",
                  stream: bool = True,
                  return_trace: bool = False):
         super().__init__(model_type=self.__class__.__name__,
                          api_key=lazyllm.config['doubao_api_key'],
                          base_url=base_url,
                          model_name=model,
-                         system_prompt=system_prompt,
                          stream=stream,
                          trainable_models=[],
                          return_trace=return_trace)
+
+    def _get_system_prompt(self):
+        return "你是人工智能助手豆包。你的任务是针对用户的问题和要求提供适当的答复和支持。"
 
     def _set_chat_url(self):
         self._url = os.path.join(self._base_url, 'chat/completions')
--- lazyllm/module/onlineChatModule/openaiModule.py
+++ lazyllm/module/onlineChatModule/openaiModule.py
@@ -14,7 +14,6 @@ class OpenAIModule(OnlineChatModuleBase, FileHandlerBase):
     def __init__(self,
                  base_url: str = "https://api.openai.com/v1",
                  model: str = "gpt-3.5-turbo",
-                 system_prompt: str = "You are a helpful assistant.",
                  stream: bool = True,
                  return_trace: bool = False,
                  **kwargs):
@@ -23,12 +22,14 @@ class OpenAIModule(OnlineChatModuleBase, FileHandlerBase):
                                       api_key=lazyllm.config['openai_api_key'],
                                       base_url=base_url,
                                       model_name=model,
-                                      system_prompt=system_prompt,
                                       stream=stream,
                                       trainable_models=OpenAIModule.TRAINABLE_MODEL_LIST,
                                       return_trace=return_trace,
                                       **kwargs)
         FileHandlerBase.__init__(self)
+
+    def _get_system_prompt(self):
+        return "You are ChatGPT, a large language model trained by OpenAI.You are a helpful assistant."
 
     def _convert_file_format(self, filepath: str) -> str:
         with open(filepath, 'r', encoding='utf-8') as fr:
