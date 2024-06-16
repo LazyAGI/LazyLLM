06af6aadecf641a2261fb33ff60a49ed6342d9c6
wangzhihong <wangzhihong@sensetime.com>
2024-06-16 17:22:36 +0800

    prompter-docs

--- lazyllm/components/prompter/chatPrompter.py
+++ lazyllm/components/prompter/chatPrompter.py
@@ -1,10 +1,10 @@
-from typing import List, Union
+from typing import List, Union, Optional
 from .builtinPrompt import LazyLLMPrompterBase
 
 class ChatPrompter(LazyLLMPrompterBase):
     def __init__(self, instruction: Union[None, str] = None,
-                 extro_keys: Union[None, List[str]] = None, show: bool = False):
-        super(__class__, self).__init__(show)
+                 extro_keys: Union[None, List[str]] = None, show: bool = False, tools: Optional[List] = None):
+        super(__class__, self).__init__(show, tools=tools)
         instruction_template = f'{instruction}\n{{extro_keys}}\n'.replace(
             '{extro_keys}', LazyLLMPrompterBase._get_extro_key_template(extro_keys))
         self._init_prompt("{sos}{system}{instruction}{tools}{eos}\n\n{history}\n{soh}\n{input}\n{eoh}{soa}\n",
