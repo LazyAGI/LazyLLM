134f707afabe8e718daf53939c54683583603f68
sunxiaoye <sunxiaoye@pjlab.org.cn>
2024-06-21 16:47:36 +0800

    lint left

--- lazyllm/components/prompter/builtinPrompt.py
+++ lazyllm/components/prompter/builtinPrompt.py
@@ -70,7 +70,7 @@ class LazyLLMPrompterBase(metaclass=LazyLLMRegisterMetaClass):
 
     def _get_instruction_and_input(self, input):
         prompt_keys = list(set(re.findall(r'\{(\w+)\}', self._instruction_template)))
-        if isinstance(input, (str, int, list)): # list for rag-nodes
+        if isinstance(input, (str, int, list)):  # list for rag-nodes
             if len(prompt_keys) == 1:
                 return self._instruction_template.format(**{prompt_keys[0]: input}), ''
             else:
