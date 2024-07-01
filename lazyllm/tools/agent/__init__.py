6796ead3b041ef424412e0a7cc99dede721f6706
wangjian <wangjian2@sensetime.com>
2024-07-01 14:02:52 +0800

    delete the commented code

--- lazyllm/tools/agent/__init__.py
+++ lazyllm/tools/agent/__init__.py
@@ -1,10 +1,7 @@
-# from .tools import LazyLLMToolBase as ToolBase, tool, ToolManager
 from .functionCall import FunctionCall
 from .toolsManager import register, ToolManager
 
 __all__ = [
-    # "ToolBase",
-    # "tool",
     "ToolManager",
     "FunctionCall",
     "register"
