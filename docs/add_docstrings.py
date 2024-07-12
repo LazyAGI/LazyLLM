cd319cca86df2d22c3e4e14652904e874c988ecb
nmhjklnm <yangchichi@126.com>
2024-07-12 18:07:27 +0800

    fix add_docstrings ignore lazynote

--- docs/add_docstrings.py
+++ docs/add_docstrings.py
@@ -1,8 +1,9 @@
-from lazynote.manager import SimpleManager
 import sys
 sys.path.append('.')
+sys.path.append('./docs/scripts')
+from lazynote.manager import SimpleManager
 import lazyllm
-manager = SimpleManager(pattern='fill', skip_on_error=True)
+manager = SimpleManager(pattern='clear', skip_on_error=True)
 manager.traverse(lazyllm, skip_modules=[
     'lazyllm.components.deploy.relay.server',
     'lazyllm.components.deploy.lmdeploy',
