dec3d6675627334b2eb59bde4b658ee70ec4b72e
yewentao256 <zhyanwentao@126.com>
2024-07-19 16:11:13 +0800

    fix: root name error

--- lazyllm/tools/rag/data_loaders.py
+++ lazyllm/tools/rag/data_loaders.py
@@ -1,5 +1,5 @@
 from typing import List
-from .store import DocNode
+from .store import DocNode, LAZY_ROOT_NAME
 from lazyllm import LOG
 
 
@@ -7,7 +7,7 @@ class DirectoryReader:
     def __init__(self, input_files: List[str]):
         self.input_files = input_files
 
-    def load_data(self, ntype: str = "root") -> List["DocNode"]:
+    def load_data(self, group: str = LAZY_ROOT_NAME) -> List["DocNode"]:
         from llama_index.core import SimpleDirectoryReader
 
         llama_index_docs = SimpleDirectoryReader(
@@ -17,7 +17,7 @@ class DirectoryReader:
         for doc in llama_index_docs:
             node = DocNode(
                 text=doc.text,
-                ntype=ntype,
+                group=group,
             )
             node.metadata = doc.metadata
             node.excluded_embed_metadata_keys = doc.excluded_embed_metadata_keys
