ed121443b75c8533645a46ff4f4df2fa966f5733
Yang Chen <yangchen@sensetime.com>
2024-07-23 17:34:23 +0800

    add test

--- tests/basic_tests/test_http_node.py
+++ tests/basic_tests/test_http_node.py
@@ -3,11 +3,27 @@ from lazyllm import LightEngine
 class TestEngine(object):
 
     def test_engine_subgraph(self):
-        nodes = [dict(id='1', kind='LocalModel', name='m1', args=dict(base_model='', deploy_method='dummy'))]
+        nodes = [dict(id='1', kind='HttpTool', name='HTTP-1', args={
+            "method": "get",
+            "url": "https://httpbin.org/ip",  # get public IP of the running server.
+            "API_Key": "",
+            "headers": {
+                "h1": "h1",
+                "h2": "h2"
+            },
+            "params": {
+                "p1": "p1",
+                "p2": "p2"
+            },
+            "body": ""
+        })]
         edges = [dict(iid='__start__', oid='1'), dict(iid='1', oid='__end__')]
 
         engine = LightEngine()
         engine.start(nodes, edges)
-        r = engine.run('1234')
-        assert 'reply for You are an AI-Agent developed by LazyLLM' in r
-        assert '1234' in r
+        r = engine.run()
+        assert 'status' in r
+
+if __name__ == '__main__':
+    http_node = TestEngine()
+    http_node.test_engine_subgraph()
