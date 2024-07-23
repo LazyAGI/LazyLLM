bdb789f55dcce752c7ac560ff78b73cce90026e2
yewentao256 <zhyanwentao@126.com>
2024-07-23 11:13:30 +0800

    bm25 and user-defined similarity

---
+++ lazyllm/tools/rag/component/bm25.py
@@ -0,0 +1,47 @@
+from typing import List, Tuple
+from ..store import DocNode
+import bm25s
+import Stemmer
+from lazyllm.thirdparty import jieba
+from .stopwords import STOPWORDS_CHINESE
+
+
+class BM25:
+    """A BM25 retriever that uses the BM25 algorithm to retrieve nodes."""
+
+    def __init__(
+        self,
+        nodes: List[DocNode],
+        language: str = "en",
+        topk: int = 2,
+        **kwargs,
+    ) -> None:
+        if language == "en":
+            self._stemmer = Stemmer.Stemmer("english")
+            self._stopwords = language
+            self._tokenizer = lambda t: t
+        elif language == "zh":
+            self._stemmer = None
+            # TODO(ywt): after bm25s supports cn stopwards, update this
+            self._stopwords = STOPWORDS_CHINESE
+            self._tokenizer = lambda t: " ".join(jieba.lcut(t))
+        self.topk = min(topk, len(nodes))
+        self.nodes = nodes
+
+        corpus_tokens = bm25s.tokenize(
+            [self._tokenizer(node.get_content()) for node in nodes],
+            stopwords=self._stopwords,
+            stemmer=self._stemmer,
+        )
+        self.bm25 = bm25s.BM25()
+        self.bm25.index(corpus_tokens)
+
+    def retrieve(self, query: str) -> List[Tuple[DocNode, float]]:
+        tokenized_query = bm25s.tokenize(
+            self._tokenizer(query), stopwords=self._stopwords, stemmer=self._stemmer
+        )
+        indexs, scores = self.bm25.retrieve(tokenized_query, k=self.topk)
+        results: List[Tuple[DocNode, float]] = []
+        for idx, score in zip(indexs[0], scores[0]):
+            results.append((self.nodes[idx], score))
+        return results
