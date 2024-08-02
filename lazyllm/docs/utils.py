a3352e9f468956a17cc74d9646b29547dc336660
unknown <liwenjian.vendor@sensetime.com>
2024-08-02 12:59:17 +0800

    update

--- lazyllm/docs/utils.py
+++ lazyllm/docs/utils.py
@@ -1,5 +1,27 @@
 import lazyllm
 
+cpp_add_doc_code = '''
+namespace py = pybind11;
+void addDocStr(py::object obj, std::string docs) {
+    static std::vector<std::string> allDocs;
+    PyObject* ptr = obj.ptr();
+    allDocs.push_back(std::move(docs));
+    if (Py_TYPE(ptr) == &PyCFunction_Type) {
+        auto f = reinterpret_cast<PyCFunctionObject*>(ptr);
+        f->m_ml->ml_doc = strdup(allDocs.back().c_str());
+        return;
+    } else if (Py_TYPE(ptr) == &PyInstanceMethod_Type) {
+        auto im = reinterpret_cast<PyInstanceMethodObject*>(ptr);
+        if (Py_TYPE(im->func) == &PyCFunction_Type) {
+            auto f = reinterpret_cast<PyCFunctionObject*>(im->func);
+            f->m_ml->ml_doc = strdup(allDocs.back().c_str());
+            return;
+        }
+    }
+    allDocs.pop_back();
+}
+'''
+
 all_examples = []
 
 def get_all_examples():   # Examples are not always exported, so process them in case of need.
