7da125a00055b317358f54c9b609d2f631436cb2
yewentao256 <zhyanwentao@126.com>
2024-08-07 10:47:16 +0800

    add unit test

--- tests/basic_tests/test_doc_impl.py
+++ tests/basic_tests/test_doc_impl.py
@@ -1,17 +1,20 @@
 from lazyllm.tools.rag.doc_impl import DocImplV2
 from lazyllm.tools.rag.transform import SentenceSplitter
-from lazyllm.tools.rag.store import DocNode
+from lazyllm.tools.rag.store import DocNode, LAZY_ROOT_NAME
 from unittest.mock import MagicMock
+import unittest
 
 
-class TestDocImplV2:
-    def setup_method(self):
+class TestDocImplV2(unittest.TestCase):
+    
+    def setUp(self):
         self.mock_embed = MagicMock()
         self.mock_directory_reader = MagicMock()
-        self.mock_directory_reader.load_data.return_value = [DocNode(text="dummy text")]
+        self.mock_directory_reader.load_data.return_value = [DocNode(uid="1", text="dummy text")]
 
         self.doc_impl = DocImplV2(embed=self.mock_embed, doc_files=["dummy_file.txt"])
         self.doc_impl.directory_reader = self.mock_directory_reader
+        self.doc_impl._lazy_init()
 
     def test_create_node_group_default(self):
         assert "CoarseChunk" in self.doc_impl.node_groups
@@ -44,3 +47,22 @@ class TestDocImplV2:
         )
         node = result[0]
         assert node.text == "dummy text"
+
+    def test_add_files(self):
+        new_docs = [DocNode(text="new dummy text")]
+        self.mock_directory_reader.load_data.return_value = new_docs
+        self.doc_impl.add_files(["new_file.txt"])
+        assert self.doc_impl.store.has_nodes(LAZY_ROOT_NAME)
+        assert len(self.doc_impl.store.traverse_nodes(LAZY_ROOT_NAME))==1
+
+    def test_delete_files(self):
+        nodes_to_delete = [DocNode(uid="1")]
+        self.mock_directory_reader.get_nodes_by_files.return_value = nodes_to_delete
+        self.doc_impl.delete_files(["dummy_file.txt"])
+
+        self.mock_directory_reader.get_nodes_by_files.assert_called_once_with(["dummy_file.txt"])
+        assert len(self.doc_impl.store.traverse_nodes(LAZY_ROOT_NAME))==0
+        
+        
+if __name__ == "__main__":
+    unittest.main()
