e32ed26168a39cad0407e0bd0eadefe2ece5c3dd
sunxiaoye <sunxiaoye@pjlab.org.cn>
2024-08-09 16:27:00 +0800

    Add multimoal chat bot example for readme

--- examples/multimodal_agent.py
+++ examples/multimodal_agent.py
@@ -1,18 +1,30 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa: F501
+
+# Three ways to specify the model:
+#   1. Specify the model name (e.g. 'internlm2-chat-7b'):
+#           the model will be automatically downloaded from the Internet;
+#   2. Specify the model name (e.g. 'internlm2-chat-7b') ​​+ set
+#      the environment variable `export LAZYLLM_MODEL_PATH="/path/to/modelzoo"`:
+#           the model will be found in `path/to/modelazoo/internlm2-chat-7b/`
+#   3. Directly pass the absolute path to TrainableModule:
+#           `path/to/modelazoo/internlm2-chat-7b`
+
 from lazyllm import TrainableModule, WebModule, deploy, pipeline, switch, _0
 
-## 写提示词:
-chatflow_intent_list = [ "聊天", "语音识别", "图片问答", "画图", "生成音乐", "文字转语音"]
+# Write prompt words:
+chatflow_intent_list = ["Chat", "Speech Recognition", "Image QA", "Drawing", "Generate Music", "Text to Speech"]
 agent_prompt = f"""
-现在你是一个意图分类引擎，负责根据对话信息分析用户输入文本并确定唯一的意图类别。\n你只需要回复意图的名字即可，不要额外输出其他字段，也不要进行翻译。"intent_list"为所有意图名列表。\n
-如果输入中带有attachments，根据attachments的后缀类型以最高优先级确定意图：如果是图像后缀如.jpg、.png等，则输出：图片问答；如果是音频后缀如.mp3、.wav等，则输出：语音识别。
-## intent_list:\n{chatflow_intent_list}\n\n## 示例\nUser: 你好啊\nAssistant:  聊天\n
+You are now an intent classification engine, responsible for analyzing user input text based on dialogue information and determining a unique intent category.\nOnly reply with the name of the intent, do not output any additional fields, and do not translate. "intent_list" is the list of all intent names.\n
+If the input contains attachments, determine the intent based on the attachment file extension with the highest priority: if it is an image extension like .jpg, .png, etc., then output: Image QA; if it is an audio extension like .mp3, .wav, etc., then output: Speech Recognition.
+## intent_list:\n{chatflow_intent_list}\n\n## Example\nUser: Hello\nAssistant: Chat
 """
-painter_prompt = f'现在你是一位绘图提示词大师，能够将用户输入的任意中文内容转换成英文绘图提示词，在本任务中你需要将任意输入内容转换成英文绘图提示词，并且你可以丰富和扩充提示词内容。'
-musician_prompt = f'现在你是一位作曲提示词大师，能够将用户输入的任意中文内容转换成英文作曲提示词，在本任务中你需要将任意输入内容转换成英文作曲提示词，并且你可以丰富和扩充提示词内容。'
-## 大语言模型:
+painter_prompt = 'Now you are a master of drawing prompts, capable of converting any Chinese content entered by the user into English drawing prompts. In this task, you need to convert any input content into English drawing prompts, and you can enrich and expand the prompt content.'
+musician_prompt = 'Now you are a master of music composition prompts, capable of converting any Chinese content entered by the user into English music composition prompts. In this task, you need to convert any input content into English music composition prompts, and you can enrich and expand the prompt content.'
+# Large language model:
 base = TrainableModule('internlm2-chat-7b').prompt(agent_prompt)
 chat = base.share().prompt()
-## 组装应用:
+# Assemble application:
 with pipeline() as ppl:
     ppl.cls = base
     ppl.cls_normalizer = lambda x: x if x in chatflow_intent_list else chatflow_intent_list[0]
@@ -23,6 +35,6 @@ with pipeline() as ppl:
         ppl.sw.case[chatflow_intent_list[3], pipeline(base.share().prompt(painter_prompt), TrainableModule('stable-diffusion-3-medium'))]
         ppl.sw.case[chatflow_intent_list[4], pipeline(base.share().prompt(musician_prompt), TrainableModule('musicgen-small'))]
         ppl.sw.case[chatflow_intent_list[5], TrainableModule('ChatTTS')]
-## 启动应用：
+# Start application:
 if __name__ == '__main__':
     WebModule(ppl, history=[chat], audio=True, port=8847).start().wait()
