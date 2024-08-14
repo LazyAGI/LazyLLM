75ed7e745e81de229b923141b78d2930b35eb4d2
wangzhihong <wangzhihong@sensetime.com>
2024-08-14 19:06:56 +0800

    docs

--- docs/gen_mkdocs_yaml.py
+++ docs/gen_mkdocs_yaml.py
@@ -6,9 +6,10 @@ assert language in ('ENGLISH', 'CHINESE')
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mkdocs.template.yml')) as f:
     content = f.read()
 
-en_default = 'true' if language=='ENGLISH' else 'false'
-zh_default = 'true' if language=='CHINESE' else 'false'
-content = content.format(en_default=en_default, zh_default=zh_default)
+doc_dir = 'en' if language == 'ENGLISH' else 'zh'
+en_default = 'true' if language == 'ENGLISH' else 'false'
+zh_default = 'true' if language == 'CHINESE' else 'false'
+content = content.format(doc_dir=doc_dir, en_default=en_default, zh_default=zh_default)
 
 with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'mkdocs.yml'), 'w+') as f:
     f.write(content)
