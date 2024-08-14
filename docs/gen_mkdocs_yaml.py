e738ca632cf921e78c471a6812169912e7dd9cce
wangzhihong <wangzhihong@sensetime.com>
2024-08-14 18:06:13 +0800

    docs

--- docs/gen_mkdocs_yaml.py
+++ docs/gen_mkdocs_yaml.py
@@ -10,5 +10,5 @@ en_default = 'true' if language=='ENGLISH' else 'false'
 zh_default = 'true' if language=='CHINESE' else 'false'
 content = content.format(en_default=en_default, zh_default=zh_default)
 
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mkdocs.yml'), 'w+') as f:
+with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'mkdocs.yml'), 'w+') as f:
     f.write(content)
