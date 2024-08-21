f72e1f4d8e6c0711839c32f96af096e77deb57d7
sunxiaoye <sunxiaoye@pjlab.org.cn>
2024-08-21 15:23:56 +0800

    Reviewer1: add refresh for config, Remove the redundant: redis_url

--- lazyllm/configs.py
+++ lazyllm/configs.py
@@ -1,6 +1,7 @@
 import os
 from enum import Enum
 import json
+from typing import List, Union
 
 
 class Mode(Enum):
@@ -11,6 +12,7 @@ class Mode(Enum):
 
 class Config(object):
     def __init__(self, prefix='LAZYLLM', home='~/.lazyllm/'):
+        self._default_add = dict()
         self.prefix = prefix
         self.impl, self.cfgs = dict(), dict()
         self.add('home', str, home, 'HOME')
@@ -38,6 +40,12 @@ class Config(object):
         return self.impl
 
     def add(self, name, type, default=None, env=None):
+        update_params = (type, default, env)
+        if name not in self._default_add or self._default_add[name] != update_params:
+            if name in self._default_add:
+                print(f"Warning: The default configuration parameter {name}({self._default_add[name]}) has been added, "
+                      f"but a new {name}({update_params}) has been added repeatedly.")
+            self._default_add.update({name: update_params})
         self.impl[name] = self.cfgs.pop(name) if name in self.cfgs else default
         if isinstance(env, dict):
             for k, v in env.items():
@@ -56,10 +64,21 @@ class Config(object):
     def __str__(self):
         return str(self.impl)
 
+    def refresh(self, targets: Union[str, List[str]] = None) -> None:
+        names = targets
+        if isinstance(targets, str):
+            names = targets.lower()
+            if names.startswith('lazyllm_'):
+                names = names[8:]
+            names = [names]
+        elif targets is None:
+            names = list(self.impl.keys())
+        assert isinstance(names, list)
+        for name in names:
+            self.add(name, *self._default_add[name])
 
 config = Config().add('mode', Mode, Mode.Normal, dict(DISPLAY=Mode.Display, DEBUG=Mode.Debug)
                 ).add('repr_ml', bool, False, 'REPR_USE_ML'
                 ).add('rag_store', str, 'none', 'RAG_STORE'
-                ).add('redis_url', str, 'none', 'REDIS_URL'
                 ).add('gpu_type', str, 'A100', 'GPU_TYPE'
                 )
