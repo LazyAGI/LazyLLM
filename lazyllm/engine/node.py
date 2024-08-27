e425be31d475bd1c6221bab36fd5cbfda6753bee
wangzhihong <wangzhihong@sensetime.com>
2024-08-27 20:02:38 +0800

    add more model

--- lazyllm/engine/node.py
+++ lazyllm/engine/node.py
@@ -14,60 +14,64 @@ class NodeArgs(object):
     getattr_f: Optional[Callable] = None
 
 
-all_nodes = dict(
+all_nodes = dict()
 
-    LocalLLM=dict(
-        module=lazyllm.TrainableModule,
-        init_arguments=dict(
-            base_model=NodeArgs(str),
-            target_path=NodeArgs(str),
-            stream=NodeArgs(bool, True),
-            return_trace=NodeArgs(bool, False)),
-        builder_argument=dict(
-            trainset=NodeArgs(str),
-            prompt=NodeArgs(str),
-            finetune_method=NodeArgs(str, getattr_f=partial(getattr, lazyllm.finetune)),
-            deploy_method=NodeArgs(str, 'vllm', getattr_f=partial(getattr, lazyllm.deploy))),
-        other_arguments=dict(
-            finetune_method=dict(
-                batch_size=NodeArgs(int, 16),
-                micro_batch_size=NodeArgs(int, 2),
-                num_epochs=NodeArgs(int, 3),
-                learning_rate=NodeArgs(float, 5e-4),
-                lora_r=NodeArgs(int, 8),
-                lora_alpha=NodeArgs(int, 32),
-                lora_dropout=NodeArgs(float, 0.05)))
-    ),
+all_nodes['LocalLLM'] = dict(
+    module=lazyllm.TrainableModule,
+    init_arguments=dict(
+        base_model=NodeArgs(str),
+        target_path=NodeArgs(str),
+        stream=NodeArgs(bool, True),
+        return_trace=NodeArgs(bool, False)),
+    builder_argument=dict(
+        trainset=NodeArgs(str),
+        prompt=NodeArgs(str),
+        finetune_method=NodeArgs(str, getattr_f=partial(getattr, lazyllm.finetune)),
+        deploy_method=NodeArgs(str, 'vllm', getattr_f=partial(getattr, lazyllm.deploy))),
+    other_arguments=dict(
+        finetune_method=dict(
+            batch_size=NodeArgs(int, 16),
+            micro_batch_size=NodeArgs(int, 2),
+            num_epochs=NodeArgs(int, 3),
+            learning_rate=NodeArgs(float, 5e-4),
+            lora_r=NodeArgs(int, 8),
+            lora_alpha=NodeArgs(int, 32),
+            lora_dropout=NodeArgs(float, 0.05)))
+)
 
-    OnlineLLM=dict(
-        module=lazyllm.OnlineChatModule,
-        init_arguments=dict(
-            source=NodeArgs(str),
-            base_model=NodeArgs(str),
-            stream=NodeArgs(bool, True),
-            return_trace=NodeArgs(bool, False)),
-        builder_argument=dict(
-            prompt=NodeArgs(str)),
-    ),
+all_nodes['OnlineLLM'] = dict(
+    module=lazyllm.OnlineChatModule,
+    init_arguments=dict(
+        source=NodeArgs(str),
+        base_model=NodeArgs(str),
+        stream=NodeArgs(bool, True),
+        return_trace=NodeArgs(bool, False)),
+    builder_argument=dict(
+        prompt=NodeArgs(str)),
+)
 
-    HttpTool=dict(
-        module=HttpRequest,
-        init_arguments=dict(
-            method=NodeArgs(str),
-            url=NodeArgs(str),
-            api_key=NodeArgs(str, ''),
-            headers=NodeArgs(dict, {}),
-            params=NodeArgs(dict, {}),
-            body=NodeArgs(str, ''),
-        ),
-        builder_argument=dict(),
-        other_arguments=dict()
-    ),
+all_nodes['VQA'] = all_nodes['SD'] = all_nodes['TTS'] = all_nodes['STT'] = dict(
+    module=lazyllm.OnlineChatModule,
+    init_arguments=dict(base_model=NodeArgs(str))
+)
 
-    Formatter=dict(
-        module=partial(getattr, lazyllm.formatter),
-        init_arguments=dict(
-            formatter=NodeArgs(str),
-        )
+all_nodes['HttpTool'] = dict(
+    module=HttpRequest,
+    init_arguments=dict(
+        method=NodeArgs(str),
+        url=NodeArgs(str),
+        api_key=NodeArgs(str, ''),
+        headers=NodeArgs(dict, {}),
+        params=NodeArgs(dict, {}),
+        body=NodeArgs(str, ''),
     ),
+    builder_argument=dict(),
+    other_arguments=dict()
+)
+
+all_nodes['Formatter'] = dict(
+    module=partial(getattr, lazyllm.formatter),
+    init_arguments=dict(
+        formatter=NodeArgs(str),
+    )
 )
