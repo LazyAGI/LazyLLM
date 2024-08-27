90d66cec1e3168d2fd877df78a7c7ce54219a638
sunxiaoye <sunxiaoye@pjlab.org.cn>
2024-08-27 14:04:08 +0800

    add llamafactory into auto

--- lazyllm/components/auto/autofinetune.py
+++ lazyllm/components/auto/autofinetune.py
@@ -11,8 +11,9 @@ class AutoFinetune(LazyLLMFinetuneBase):
                 batch_size=32, lora_r=8, launcher=launchers.remote(ngpus=1), **kw):
         base_model = ModelManager(source).download(base_model)
         model_name = get_model_name(base_model)
-        if ModelManager.get_model_type(model_name) == 'embed':
-            raise RuntimeError('Fine-tuning of the embed model is not currently supported.')
+        model_type = ModelManager.get_model_type(model_name)
+        if model_type in ['embed', 'tts', 'vlm', 'stt', 'sd']:
+            raise RuntimeError(f'Fine-tuning of the {model_type} model is not currently supported.')
         map_name = model_map(model_name)
         base_name = model_name.split('-')[0].split('_')[0].lower()
         candidates = get_configer().query_finetune(lazyllm.config['gpu_type'], launcher.ngpus,
@@ -28,6 +29,8 @@ class AutoFinetune(LazyLLMFinetuneBase):
                 for key, value in finetune_cls.auto_map.items():
                     if value:
                         kw[value] = getattr(c, key)
+                if finetune_cls.__name__ == 'LlamafactoryFinetune':
+                    return finetune_cls(base_model, target_path, lora_r=lora_r, launcher=launcher, **kw)
                 return finetune_cls(base_model, target_path, merge_path, cp_files='tokeniz*',
                                     batch_size=batch_size, lora_r=lora_r, launcher=launcher, **kw)
         raise RuntimeError(f'No valid framework found, candidates are {[c.framework.lower() for c in candidates]}')
