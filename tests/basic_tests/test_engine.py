c2eb6b93074f3d2e83a89f7da606cc2455d05942
wangzhihong <wangzhihong@sensetime.com>
2024-08-28 11:01:54 +0800

    add test for formatter

--- tests/basic_tests/test_engine.py
+++ tests/basic_tests/test_engine.py
@@ -40,3 +40,17 @@ class TestEngine(object):
         assert engine.run(1) == 2
         assert engine.run(2) == 6
         assert engine.run(3) == 9
+
+    def test_engine_formatter(self):
+        nodes = [dict(id='1', kind='Code', name='m1', args='def test(x: int):\n    return x\n'),
+                 dict(id='2', kind='Code', name='m2', args='def test(x: int):\n    return [[x, 2*x], [3*x, 4*x]]\n'),
+                 dict(id='3', kind='Code', name='m3', args='def test(x: int):\n    return dict(a=1, b=x * x)\n'),
+                 dict(id='4', kind='Code', name='m4', args='def test(x, y, z):\n    return f"{x}{y}{z}"\n')]
+        edges = [dict(iid='__start__', oid='1'), dict(iid='__start__', oid='2'), dict(iid='__start__', oid='3'),
+                 dict(iid='1', oid='4'), dict(iid='2', oid='4', formatter='[:, 1]'),
+                 dict(iid='3', oid='4', formatter='[b]'), dict(iid='4', oid='__end__')]
+
+        engine = LightEngine()
+        engine.start(nodes, edges)
+        assert engine.run(1) == '1[2, 4]1'
+        assert engine.run(2) == '2[4, 8]4'
