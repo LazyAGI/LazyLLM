c80c11a1f5aab174ae04f8fd24be4c05757ab964
wangzhihong <wangzhihong@sensetime.com>
2024-09-01 16:23:12 +0800

    add lazyllm run script

--- lazyllm/cli/run.py
+++ lazyllm/cli/run.py
@@ -1,5 +1,13 @@
 import sys
+import argparse
+
+# lazyllm run xx.json / xx.dsl / xx.lazyml
+# lazyllm run chatbot --model=xx --framework=xx --source=xx
+# lazyllm run rag --model=xx --framework=xx --source=xx --documents=''
 
 def run(commands):
+    parser = argparse.ArgumentParser(description="lazyllm deploy command")
+    parser.add_argument("command", help="model name")
+
     print('lazyllm run is not ready yet.')
     sys.exit(0)
