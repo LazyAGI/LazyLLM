33fe24d5b17f5f0c5f966ab66b8776bbb2679b57
wangzhihong <wangzhihong@sensetime.com>
2024-09-03 10:56:40 +0800

    fix some bugs and update version to v0.2.2

--- lazyllm/cli/run.py
+++ lazyllm/cli/run.py
@@ -10,13 +10,13 @@ def chatbot(llm):
     lazyllm.WebModule(llm, port=range(20000, 25000)).start().wait()
 
 
-def rag(llm, documents):
+def rag(llm, docpath):
     import lazyllm
     from lazyllm import pipeline, parallel, bind, SentenceSplitter, Document, Retriever, Reranker
     prompt = ('You will play the role of an AI Q&A assistant and complete a dialogue task. In this '
               'task, you need to provide your answer based on the given context and question.')
 
-    documents = Document(dataset_path="rag_master", embed=lazyllm.OnlineEmbeddingModule(), create_ui=False)
+    documents = Document(dataset_path=docpath, embed=lazyllm.OnlineEmbeddingModule(), create_ui=False)
     documents.create_node_group(name="sentences", transform=SentenceSplitter, chunk_size=1024, chunk_overlap=100)
 
     with pipeline() as ppl:
