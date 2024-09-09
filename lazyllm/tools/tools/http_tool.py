00a9caa89cb93c76513ce8bfd82c43df3af4afd4
ouguoyu <ouguoyu@sensetime.com>
2024-09-09 10:51:20 +0800

    s

--- lazyllm/tools/tools/http_tool.py
+++ lazyllm/tools/tools/http_tool.py
@@ -6,14 +6,13 @@ class HttpTool(HttpRequest):
     def __init__(self,
                  method: Optional[str] = None,
                  url: Optional[str] = None,
-                 api_key: Optional[str] = None,
                  headers: Optional[Dict] = None,
                  params: Optional[Dict] = None,
                  body: Optional[str] = None,
                  timeout: int = 10,
                  proxies: Optional[Dict] = None,
                  py_code: Optional[str] = None):
-        super().__init__(method, url, api_key, headers, params, body)
+        super().__init__(method, url, '', headers, params, body)
         self._has_http = True if url else False
         self._py_code = compile_code(py_code) if py_code else None
 
