5eb93256c124540d985382442e9923a844c11aec
wenduren <wenduren@sensetime.com>
2024-09-12 16:35:33 +0800

    update website assistant - show source & copy code

--- docs/assets/js/assistant.js
+++ docs/assets/js/assistant.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/showdown/dist/showdown.min.js';
+    script.src = 'https://cdn.jsdelivr.net/npm/showdown/dist/showdown.min.js'
     document.head.appendChild(script);
     
     var script = document.createElement('script');
-    script.src = 'https://docs.lazyllm.ai/en/latest/assets/js/jigsaw.js';
+    script.src = 'assets/js/jigsaw.js'
     document.head.appendChild(script);
     // 对话助手入口
     var floatingIcon = document.createElement('div');
@@ -12,11 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
     floatingIcon.innerHTML = '<img src="https://docs.lazyllm.ai/en/latest/assets/logo.png" alt="icon">';
     document.body.appendChild(floatingIcon);
     // 对话框
-    var placeholdervalue = 'What is LazyLLM ?';
+    var placeholdervalue = 'What is LazyLLM ?'
     var dialogBox = document.createElement('div');
     dialogBox.id = 'dialog-box';
-    dialogBox.style = 'display: none';
+    dialogBox.style = 'display: none'
     dialogBox.innerHTML = `
+    <div class="dialog-container">
         <div id="dialog" class="dialog"> 
             <div id="reset-dialog" class="reset"></div>
             <div id="close-dialog-box" class="close"></div>
@@ -26,19 +27,20 @@ document.addEventListener('DOMContentLoaded', function() {
         <div class="input-container"> 
             <input type="text" id="user-input" placeholder="What is LazyLLM ?"> 
             <button id="send-message">Send</button> 
-        </div>`;
+        </div>
+    </div>`
 
     document.body.appendChild(dialogBox);
     // 人机验证框
     var captchaBox = document.createElement('div');
     captchaBox.id = 'captcha-box';
-    captchaBox.style = "display: none";
+    captchaBox.style = "display: none"
     captchaBox.innerHTML = `
     <div id="close-captcha" class="close-captcha"></div>
     <div id="captcha-container" class="container">
         <div id="captcha"></div>
     </div>
-    `;
+    `
     document.body.appendChild(captchaBox);
 
     const sendMessageButton = document.getElementById('send-message');
@@ -48,10 +50,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const messages = document.getElementById('messages');
     const closeCaptchaButton = document.getElementById('close-captcha');
 
-    let userQuery = '';
+    var userQuery = '';
     var isChecked = false;
+    var isWaiting = false;
+    var firstHello = true;
+    var showSource = true;
     var converter;
-
+    
     function setIsCheckedT() {
         isChecked = true;
     }
@@ -60,14 +65,22 @@ document.addEventListener('DOMContentLoaded', function() {
         isChecked = false;
     }
 
+    function setMessageState(state) {
+        isWaiting = state;
+        sendMessageButton.disabled = state;
+    }
+    
     function popHelloMessage() {
-        let hello = document.createElement('div');
-        hello.innerHTML = `
-                <div class="message received">  
-                    <img src="https://docs.lazyllm.ai/en/latest/assets/logo.png" class="avatar"> 
-                    <p>Hello, how can I assist you today ?</p> 
-                </div> `
-        messages.appendChild(hello);
+        if (!isWaiting && firstHello){
+            let hello = document.createElement('div');
+            hello.innerHTML = `
+                    <div class="message received">  
+                        <img src="https://docs.lazyllm.ai/en/latest/assets/logo.png" class="avatar"> 
+                        <p>Hello, how can I assist you today ?</p> 
+                    </div> `
+            messages.appendChild(hello)
+            firstHello = false;
+        }
     }
 
     function cleanToken() {
@@ -75,20 +88,31 @@ document.addEventListener('DOMContentLoaded', function() {
         setIsCheckedF();
     }
 
+    function handleClickOutside(event) {
+        if (event.target === dialogBox) {
+            dialogBox.style.display = 'none';
+            // 移除点击事件监听器
+            window.removeEventListener('click', handleClickOutside);
+        } 
+    }
+
     function switchDialobBoxDisplay() {
         dialogBox.style.display = dialogBox.style.display==='none' ? 'block' : 'none';
         if (dialogBox.style.display === 'none') {
             captchaBox.style.display = 'none';
+            window.removeEventListener('click', handleClickOutside);
         } else {
+            window.addEventListener('click', handleClickOutside);
             setTimeout(popHelloMessage, 1000);
         }
     }
 
     function resetDialog(){
         messages.innerHTML = '';
-        sendMessageButton.disabled = false;
-        setTimeout(popHelloMessage, 1000);
+        setMessageState(false);
         cleanToken();
+        firstHello = true;
+        setTimeout(popHelloMessage, 1000);
     }
 
     async function Verify() {
@@ -97,7 +121,7 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             let seed = Math.ceil(Math.random()*10000);
             try {
-                const response = await fetch('https://api.lazyllm.ai/authorize', {
+                const response = await fetch('http://lazyllm.top/authorize', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -135,6 +159,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function closeCaptcha() {
         captchaBox.style.display = 'none';
+        setMessageState(false);
         userInput.value = userQuery;
         messages.removeChild(messages.lastChild);
     }
@@ -146,13 +171,18 @@ document.addEventListener('DOMContentLoaded', function() {
         botMessage.innerHTML = `<img src="assets/logo.png" alt="机器人头像" class="avatar">
                                 <div class="content">
                                     <div id="loader"></div>
-                                </div>`;
+                                </div>
+                                `;
+        botMessage.children[1].addEventListener('click', function(e) {
+            console.log(e);
+            navigator.clipboard.writeText(e.target.innerText);
+        });
         messages.appendChild(botMessage);
         messages.scrollTop = messages.scrollHeight; // 滚动到最新消息
 
         try {
             // 发送请求到后端接口
-            const response = await fetch('https://api.lazyllm.ai/chat', {
+            const response = await fetch('http://lazyllm.top/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -162,33 +192,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // 处理响应
             const data = await response.json();
+            converter = new showdown.Converter();
             if (data.errmsg == 200){
                 botMessage.children[1].innerHTML = converter.makeHtml(data.reply);
-            } else if (data.errmsg == 101){
-                botMessage.children[1].innerHTML = converter.makeHtml("未能获取回复");
+                if (showSource){
+                    var source_box = document.createElement('div');
+                    source_box.innerHTML = converter.makeHtml(data.sources);
+                    botMessage.children[1].appendChild(source_box);
+                }
+            } else if (data.errmsg == 102 || data.errmsg==103){
+                botMessage.children[1].innerHTML = converter.makeHtml(data.reply);
+            } else {
+                botMessage.children[1].innerHTML = converter.makeHtml("未能获取回复，请稍后再试");
                 cleanToken();
                 closeCaptcha();
-            } else {
-                botMessage.children[1].innerHTML = converter.makeHtml("未能获取回复");
-                botMessage.children[1].innerHTML = converter.makeHtml(data.reply);
             }
-                
-            userQuery = '';
-            sendMessageButton.disabled = false;
-            // 显示加载状态或发送请求到后端接口
         } catch (error) {
             botMessage.children[1].innerHTML = converter.makeHtml('请求失败');
         }
-
+        userQuery = '';
+        setMessageState(false);
         messages.scrollTop = messages.scrollHeight; // 滚动到最新消息
     }
     
     function sendMessage() {
-        const text = userInput.value.trim();
-        if (!text) {
-            userInput.value = placeholdervalue;
+        if (isWaiting){
+            return;
         }
-        if (text) {
+        const text = userInput.value.trim();
+        if (!text) { userInput.value = placeholdervalue; }
+        else {
             const userMessage = document.createElement('div');
             userMessage.classList.add('message');
             userMessage.classList.add('sent');
@@ -201,7 +234,8 @@ document.addEventListener('DOMContentLoaded', function() {
             // 清空输入框
             userQuery = text;
             userInput.value = '';
-            sendMessageButton.disabled = true;
+            
+            setMessageState(true);
 
             if (!isChecked){
                 showCaptcha();
@@ -221,4 +255,5 @@ document.addEventListener('DOMContentLoaded', function() {
             sendMessage();
         }
     });
+
 });
