0c7582c47c904dbb973470f9ceecf43d7ca1a3fa
wangzhihong <wangzhihong@sensetime.com>
2024-09-27 14:14:09 +0800

    parallel transform for doc

--- lazyllm/tools/rag/index.py
+++ lazyllm/tools/rag/index.py
@@ -65,9 +65,9 @@ class DefaultIndex:
                 for k in miss_keys:
                     with node._lock:
                         if node.has_missing_embedding(k):
-                            node.embedding_state.add(k)
                             future = executor.submit(node.do_embedding, {k: self.embed[k]}) \
                                 if k not in node.embedding_state else executor.submit(node.check_embedding_state, k)
+                            node.embedding_state.add(k)
                             futures.append(future)
             if len(futures) > 0:
                 for future in concurrent.futures.as_completed(futures):
