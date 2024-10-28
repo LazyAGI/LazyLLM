094a57306f158ebe60bfe0d99220e3054f403c7e
GY <856454+ouonline@users.noreply.github.com>
2024-10-28 16:58:06 +0800

    bugfix: RewooAgent does not convert input types (#323)

--- tests/advanced_tests/standard_test/test_tools_manager.py
+++ tests/advanced_tests/standard_test/test_tools_manager.py
@@ -1,13 +1,23 @@
 import re
 import docstring_parser
+import lazyllm
 from lazyllm.tools import ToolManager
-from pydantic import BaseModel
-from typing import Literal
+from lazyllm.tools.agent.toolsManager import (
+    _gen_empty_func_str_from_parsed_docstring,
+    _gen_func_from_str,
+    _check_return_type_is_the_same,
+    register,
+)
+from lazyllm.common import LazyLLMRegisterMetaClass
+from typing import Literal, get_type_hints
 
-# TODO user defined types must inherit from `BaseModel`
-class MyClass(BaseModel):
-    pass
-
+def _gen_wrapped_moduletool(func):
+    if "tmp_tool" not in LazyLLMRegisterMetaClass.all_clses:
+        register.new_group('tmp_tool')
+    register('tmp_tool')(func)
+    wrapped_module = getattr(lazyllm.tmp_tool, func.__name__)()
+    lazyllm.tmp_tool.remove(func.__name__)
+    return wrapped_module
 
 class TestToolManager:
     def test_gen_empty_func_str_from_parsed_docstring(self):
@@ -24,7 +34,7 @@ class TestToolManager:
         '''
 
         parsed_doc = docstring_parser.parse(var_args_doc)
-        func_str = ToolManager._gen_empty_func_str_from_parsed_docstring(parsed_doc)
+        func_str = _gen_empty_func_str_from_parsed_docstring(parsed_doc)
         expected_pattern = "def f[0-9]+\(a:int,b:Literal\['foo', 'bar', 'baz'\],c:List\[str\],\)->str:\n    pass"  # noqa W605
         res = re.match(expected_pattern, func_str)
         assert res.span()
@@ -38,40 +48,10 @@ class TestToolManager:
         Args:
             v (int): value
         '''
-        func = ToolManager._gen_func_from_str(func_str1, func_doc1)
+        func = _gen_func_from_str(func_str1, func_doc1)
         value = 5
         assert func(value) == (value + 1)
         assert func.__doc__ == func_doc1
-
-    def test_gen_args_info_from_moduletool_and_docstring(self):
-        def add3(v: int) -> int:
-            '''
-            this is a function adding 3 to the value.
-
-            Args:
-                v (int): this is v's desc in function, different from that in doc
-
-            Returns:
-                int: value+3
-            '''
-            return v + 3
-
-        add3_doc1 = '''
-        this is a function adding 3 to the value.
-
-        Args:
-            v (int): this is v's desc
-
-        Returns:
-            int: value+3
-        '''
-
-        tool = ToolManager._gen_wrapped_moduletool(add3)
-        parsed_doc = docstring_parser.parse(add3_doc1)
-        args = ToolManager._gen_args_info_from_moduletool_and_docstring(tool, parsed_doc)
-        info_of_v = args['v']
-        assert info_of_v['type'] == 'integer'
-        assert info_of_v['description'] == "this is v's desc"
 
     def test_enum_func(self):
         def func(s: Literal["a", "b", "c"]):
@@ -87,12 +67,12 @@ class TestToolManager:
             s (Literal['a', 'b', 'c']): a string
         '''
         parsed_doc = docstring_parser.parse(doc)
-        tool = ToolManager._gen_wrapped_moduletool(func)
+        tool = _gen_wrapped_moduletool(func)
         args = ToolManager._gen_args_info_from_moduletool_and_docstring(tool, parsed_doc)
         info_of_s = args['s']
         assert info_of_s['enum'] == ['a', 'b', 'c']
 
-    def test_check_return_info_is_the_same(self):
+    def test_check_return_type_is_the_same(self):
         def add5(v: int) -> int:
             '''
             this is a function adding 5 to the value.
@@ -116,9 +96,18 @@ class TestToolManager:
         '''
 
         func_str = 'def add5(v:int) -> int:\n    return v+5'
-        func_from_doc = ToolManager._gen_func_from_str(func_str, add5_doc1)
-        tool = ToolManager._gen_wrapped_moduletool(add5)
-        assert ToolManager._check_return_info_is_the_same(func_from_doc, tool)
+        func_from_doc = _gen_func_from_str(func_str, add5_doc1)
+        tool = _gen_wrapped_moduletool(add5)
+        try:
+            value = 123
+            _check_return_type_is_the_same(
+                get_type_hints(func_from_doc, globals(), locals()),
+                get_type_hints(tool.__call__, globals(), locals()))
+            value = 456
+        except Exception:
+            value = 789
+        finally:
+            assert value == 456
 
         # ----- #
 
@@ -133,8 +122,17 @@ class TestToolManager:
             str: c itself
         """
 
-        func_from_doc2 = ToolManager._gen_func_from_str(str_without_arg_type, doc2)
-        assert not ToolManager._check_return_info_is_the_same(func_from_doc2, tool)
+        func_from_doc2 = _gen_func_from_str(str_without_arg_type, doc2)
+        try:
+            value = 123
+            _check_return_type_is_the_same(
+                get_type_hints(func_from_doc2, globals(), locals()),
+                get_type_hints(tool.__call__, globals(), locals()))
+            value = 456
+        except Exception:
+            value = 789
+        finally:
+            assert value == 456
 
         # ----- #
 
@@ -149,8 +147,17 @@ class TestToolManager:
             str: c itself
         """
 
-        func_from_doc3 = ToolManager._gen_func_from_str(str_without_return_type, doc3)
-        assert not ToolManager._check_return_info_is_the_same(func_from_doc3, tool)
+        func_from_doc3 = _gen_func_from_str(str_without_return_type, doc3)
+        try:
+            value = 123
+            _check_return_type_is_the_same(
+                get_type_hints(func_from_doc3, globals(), locals()),
+                get_type_hints(tool.__call__, globals(), locals()))
+            value = 456
+        except Exception:
+            value = 789
+        finally:
+            assert value == 456
 
     def test_invalid_typing(self):
         invalid_doc1 = '''
@@ -164,10 +171,10 @@ class TestToolManager:
         '''
 
         parsed_docstring = docstring_parser.parse(invalid_doc1)
-        func_str = ToolManager._gen_empty_func_str_from_parsed_docstring(parsed_docstring)
+        func_str = _gen_empty_func_str_from_parsed_docstring(parsed_docstring)
         try:
             test_value = 123
-            ToolManager._gen_func_from_str(func_str, invalid_doc1)
+            _gen_func_from_str(func_str, invalid_doc1)
             test_value = 456
         except Exception:
             test_value = 789
@@ -186,35 +193,12 @@ class TestToolManager:
         '''
 
         parsed_docstring = docstring_parser.parse(invalid_doc1)
-        func_str = ToolManager._gen_empty_func_str_from_parsed_docstring(parsed_docstring)
+        func_str = _gen_empty_func_str_from_parsed_docstring(parsed_docstring)
         try:
             test_value = 111
-            ToolManager._gen_func_from_str(func_str, invalid_doc1)
+            _gen_func_from_str(func_str, invalid_doc1)
             test_value = 222
         except Exception:
             test_value = 333
         finally:
             assert test_value == 333
-
-    def test_user_defined_type(self):
-        valid_doc = '''
-        this is a doc containing user defined types.
-
-        Args:
-            mc (MyClass): an instance of `MyClass`
-        '''
-
-        parsed_docstring = docstring_parser.parse(valid_doc)
-
-        func_from_doc = ToolManager._gen_func_from_str(
-            ToolManager._gen_empty_func_str_from_parsed_docstring(parsed_docstring),
-            valid_doc, {'MyClass': MyClass})
-        tool = ToolManager._gen_wrapped_moduletool(func_from_doc)
-
-        func_str = "def ddd(mc: MyClass):\n    pass"
-        func_from_str = ToolManager._gen_func_from_str(func_str, valid_doc, {'MyClass': MyClass})
-        assert ToolManager._check_return_info_is_the_same(func_from_str, tool)
-
-        func_str2 = "def ddd2(mc: MyClass) -> int:\n    pass"
-        func_from_str2 = ToolManager._gen_func_from_str(func_str2, valid_doc, {'MyClass': MyClass})
-        assert not ToolManager._check_return_info_is_the_same(func_from_str2, tool)
