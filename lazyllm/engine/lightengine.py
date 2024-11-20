6b1b3aad55f2ffb3b43919d5422657702f9f7990
ouguoyu <ouguoyu@sensetime.com>
2024-11-20 19:57:09 +0800

    s

--- lazyllm/engine/lightengine.py
+++ lazyllm/engine/lightengine.py
@@ -120,7 +120,7 @@ class LightEngine(Engine):
             assert isinstance(f, ServerGraph), 'Only graph can support history'
             if not isinstance(history, list) and all([isinstance(h, list) for h in history]):
                 raise RuntimeError('History shoule be [[str, str], ..., [str, str]] (list of list of str)')
-            lazyllm.globals['chat_history'] = {Engine().build_node(i).func._module_id: history for i in ids}
+            lazyllm.globals['chat_history'] = {Engine().build_node(i).func._module_id: history for i in f._history_ids}
         result = self.build_node(id).func(*args, **kw)
         lazyllm.globals['lazyllm_files'] = {}
         lazyllm.globals['chat_history'] = {}
