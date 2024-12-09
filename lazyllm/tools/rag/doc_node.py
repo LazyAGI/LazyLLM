239da57347cb4bf5ac24a858381db48bc5fcbd6c
wangzhihong <wangzhihong@sensetime.com>
2024-12-09 18:11:44 +0800

    Add QA-DocNode (#383)

--- lazyllm/tools/rag/doc_node.py
+++ lazyllm/tools/rag/doc_node.py
@@ -23,34 +23,58 @@ class DocNode:
         if text and content:
             raise ValueError('`text` and `content` cannot be set at the same time.')
 
-        self.uid: str = uid if uid else str(uuid.uuid4())
-        self.content: Optional[Union[str, List[Any]]] = content if content else text
-        self.group: Optional[str] = group
-        self.embedding: Optional[Dict[str, List[float]]] = embedding or {}
+        self._uid: str = uid if uid else str(uuid.uuid4())
+        self._content: Optional[Union[str, List[Any]]] = content if content else text
+        self._group: Optional[str] = group
+        self._embedding: Optional[Dict[str, List[float]]] = embedding or {}
         self._metadata: Dict[str, Any] = metadata or {}
         # Metadata keys that are excluded from text for the embed model.
         self._excluded_embed_metadata_keys: List[str] = []
         # Metadata keys that are excluded from text for the LLM.
         self._excluded_llm_metadata_keys: List[str] = []
-        self.parent: Optional["DocNode"] = parent
-        self.children: Dict[str, List["DocNode"]] = defaultdict(list)
+        self._parent: Optional["DocNode"] = parent
+        self._children: Dict[str, List["DocNode"]] = defaultdict(list)
         self._lock = threading.Lock()
         self._embedding_state = set()
 
         if global_metadata and parent:
             raise ValueError('only ROOT node can set global metadata.')
-        self._global_metadata = global_metadata if global_metadata else {}
+        self._global_metadata = global_metadata or {}
 
     @property
     def text(self) -> str:
-        if isinstance(self.content, str):
-            return self.content
-        elif isinstance(self.content, list):
-            if not all([isinstance(ele, str) for ele in self.content]):
-                raise TypeError("Found non-string element in content")
-            return '\n'.join(self.content)
+        if isinstance(self._content, str):
+            return self._content
+        elif isinstance(self._content, list):
+            if unexcepted := set([type(ele) for ele in self._content if not isinstance(ele, str)]):
+                raise TypeError(f"Found non-string element in content: {unexcepted}")
+            return '\n'.join(self._content)
         else:
-            raise TypeError(f"content type '{type(self.content)}' is neither a str nor a list")
+            raise TypeError(f"content type '{type(self._content)}' is neither a str nor a list")
+
+    @property
+    def embedding(self):
+        return self._embedding
+
+    @embedding.setter
+    def embedding(self, v: Optional[Dict[str, List[float]]]):
+        self._embedding = v
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, v: Optional["DocNode"]):
+        self._parent = v
+
+    @property
+    def children(self):
+        return self._children
+
+    @children.setter
+    def children(self, v: Dict[str, List["DocNode"]]):
+        self._children = v
 
     @property
     def root_node(self) -> Optional["DocNode"]:
@@ -71,7 +95,7 @@ class DocNode:
 
     @property
     def metadata(self) -> Dict:
-        return self.root_node._metadata
+        return {**self.root_node._metadata, **self._metadata}
 
     @metadata.setter
     def metadata(self, metadata: Dict) -> None:
@@ -79,7 +103,7 @@ class DocNode:
 
     @property
     def excluded_embed_metadata_keys(self) -> List:
-        return self.root_node._excluded_embed_metadata_keys
+        return list(set(self.root_node._excluded_embed_metadata_keys + self._excluded_embed_metadata_keys))
 
     @excluded_embed_metadata_keys.setter
     def excluded_embed_metadata_keys(self, excluded_embed_metadata_keys: List) -> None:
@@ -87,7 +111,7 @@ class DocNode:
 
     @property
     def excluded_llm_metadata_keys(self) -> List:
-        return self.root_node._excluded_llm_metadata_keys
+        return list(set(self.root_node._excluded_llm_metadata_keys + self._excluded_llm_metadata_keys))
 
     @excluded_llm_metadata_keys.setter
     def excluded_llm_metadata_keys(self, excluded_llm_metadata_keys: List) -> None:
@@ -104,28 +128,28 @@ class DocNode:
 
     def get_children_str(self) -> str:
         return str(
-            {key: [node.uid for node in nodes] for key, nodes in self.children.items()}
+            {key: [node._uid for node in nodes] for key, nodes in self.children.items()}
         )
 
     def get_parent_id(self) -> str:
-        return self.parent.uid if self.parent else ""
+        return self.parent._uid if self.parent else ""
 
     def __str__(self) -> str:
         return (
-            f"DocNode(id: {self.uid}, group: {self.group}, content: {self.content}) parent: {self.get_parent_id()}, "
+            f"DocNode(id: {self._uid}, group: {self._group}, content: {self._content}) parent: {self.get_parent_id()}, "
             f"children: {self.get_children_str()}"
         )
 
     def __repr__(self) -> str:
-        return str(self) if config["debug"] else f'<Node id={self.uid}>'
+        return str(self) if config["debug"] else f'<Node id={self._uid}>'
 
     def __eq__(self, other):
         if isinstance(other, DocNode):
-            return self.uid == other.uid
+            return self._uid == other._uid
         return False
 
     def __hash__(self):
-        return hash(self.uid)
+        return hash(self._uid)
 
     def has_missing_embedding(self, embed_keys: Union[str, List[str]]) -> List[str]:
         if isinstance(embed_keys, str): embed_keys = [embed_keys]
@@ -174,4 +198,17 @@ class DocNode:
         return f"{metadata_str}\n\n{self.text}".strip()
 
     def to_dict(self) -> Dict:
-        return dict(content=self.content, embedding=self.embedding, metadata=self.metadata)
+        return dict(content=self._content, embedding=self.embedding, metadata=self.metadata)
+
+
+class QADocNode(DocNode):
+    def __init__(self, query: str, answer: str, uid: Optional[str] = None, group: Optional[str] = None,
+                 embedding: Optional[Dict[str, List[float]]] = None, parent: Optional["DocNode"] = None,
+                 metadata: Optional[Dict[str, Any]] = None, *, text: Optional[str] = None):
+        super().__init__(uid, query, group, embedding, parent, metadata, None, text=text)
+        self._answer = answer.strip()
+
+    def get_text(self, metadata_mode: MetadataMode = MetadataMode.NONE) -> str:
+        if metadata_mode == MetadataMode.LLM:
+            return f'query:\n{self.text}\nanswer\n{self._answer}'
+        return super().get_text(metadata_mode)
--- tests/advanced_tests/standard_test/test_llm_parser.py
+++ tests/advanced_tests/standard_test/test_llm_parser.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import MagicMock
 from lazyllm import LLMParser, TrainableModule
 from lazyllm.launcher import cleanup
+from lazyllm.tools.rag import DocNode
 
 
 class TestLLMParser(unittest.TestCase):
@@ -16,6 +17,7 @@ class TestLLMParser(unittest.TestCase):
 
         cls.summary_parser = LLMParser(cls.llm, language="en", task_type="summary")
         cls.keywords_parser = LLMParser(cls.llm, language="en", task_type="keywords")
+        cls.qa_parser = LLMParser(cls.llm, language="en", task_type="qa")
 
     @classmethod
     def teardown_class(cls):
@@ -25,15 +27,24 @@ class TestLLMParser(unittest.TestCase):
         result = self.summary_parser.transform(self.mock_node)
         assert isinstance(result, list)
         assert isinstance(result[0], str)
-        assert len(result[0]) < 100
+        assert len(result[0]) < 150
         assert "LazyLLM" in result[0]
 
     def test_keywords_transform(self):
         result = self.keywords_parser.transform(self.mock_node)
         assert isinstance(result, list)
+        assert 1 < len(result) < 10
         assert isinstance(result[0], str)
-        assert len(result[0]) < 100
-        assert "LazyLLM," in result[0]
+        assert len(result[0]) < 20
+        assert "LazyLLM" in result
+
+    def test_qa_transform(self):
+        result = self.qa_parser.transform(self.mock_node)
+        assert isinstance(result, list)
+        assert isinstance(result[0], DocNode)
+        text, content = result[0].text, result[0].get_content()
+        assert len(text) < len(content) and text in content
+        assert 'query:' in content and 'answer' in content
 
 
 if __name__ == "__main__":
