272a4e8027a54ba18ddadec57de01dbbbdb506de
sunxiaoye <sunxiaoye@pjlab.org.cn>
2024-12-17 15:29:48 +0800

    New download progress

--- lazyllm/components/utils/downloader/model_downloader.py
+++ lazyllm/components/utils/downloader/model_downloader.py
@@ -1,5 +1,9 @@
 import os
+import time
 import shutil
+import threading
+from abc import ABC, abstractmethod
+
 import lazyllm
 from .model_mapping import model_name_mapping, model_provider, model_groups
 from lazyllm.common.common import EnvVarContextManager
@@ -18,7 +22,7 @@ class ModelManager():
                  cache_dir=lazyllm.config['model_cache_dir'],
                  model_path=lazyllm.config['model_path']):
         self.model_source = model_source
-        self.token = token
+        self.token = token or None
         self.cache_dir = cache_dir
         self.model_paths = model_path.split(":") if len(model_path) > 0 else []
 
@@ -70,7 +74,7 @@ class ModelManager():
                     "source": {k: v + '/' + model_base for k, v in model_provider[matched_model_prefix].items()}
                 }
 
-    def download(self, model=''):
+    def download(self, model='', call_back=None):
         assert isinstance(model, str), "model name should be a string."
         self._try_add_mapping(model)
         # Dummy or local model.
@@ -88,7 +92,7 @@ class ModelManager():
             full_model_dir = os.path.join(self.cache_dir, model)
 
             mapped_model_name = model_name_mapping[model.lower()]['source'][self.model_source]
-            model_save_dir = self._do_download(mapped_model_name)
+            model_save_dir = self._do_download(mapped_model_name, call_back)
             if model_save_dir:
                 # The code safely creates a symbolic link by removing any existing target.
                 if os.path.exists(full_model_dir):
@@ -106,8 +110,26 @@ class ModelManager():
             if matched_model_prefix and self.model_source in model_provider[matched_model_prefix]:
                 model_name_for_download = model_provider[matched_model_prefix][self.model_source] + '/' + model
 
-            model_save_dir = self._do_download(model_name_for_download)
+            model_save_dir = self._do_download(model_name_for_download, call_back)
             return model_save_dir if model_save_dir else model
+
+    def validate_token(self):
+        if self.model_source == 'huggingface':
+            return HuggingfaceDownloader(token=self.token).verify_hub_token()
+        elif self.model_source == 'modelscope':
+            return ModelscopeDownloader(token=self.token).verify_hub_token()
+        else:
+            lazyllm.LOG.warning("Only support Huggingface and Modelscope currently.")
+            return False
+
+    def validate_model_id(self, model_id):
+        if self.model_source == 'huggingface':
+            return HuggingfaceDownloader(token=self.token).verify_model_id(model_id)
+        elif self.model_source == 'modelscope':
+            return ModelscopeDownloader(token=self.token).verify_model_id(model_id)
+        else:
+            lazyllm.LOG.warning("Only support Huggingface and Modelscope currently.")
+            return False
 
     def _model_exists_at_path(self, model_name):
         if len(self.model_paths) == 0:
@@ -137,18 +159,18 @@ class ModelManager():
             return False
         return any((True for _ in os.scandir(model_dir)))
 
-    def _do_download(self, model=''):
+    def _do_download(self, model='', call_back=None):
         model_dir = model.replace('/', os.sep)
         full_model_dir = os.path.join(self.cache_dir, self.model_source, model_dir)
 
         try:
             if self.model_source == 'huggingface':
-                return self._download_model_from_hf(model, full_model_dir)
+                return HuggingfaceDownloader(call_back, self.token).download(model, full_model_dir)
             elif self.model_source == 'modelscope':
-                return self._download_model_from_ms(model, full_model_dir)
+                return ModelscopeDownloader(call_back, self.token).download(model, full_model_dir)
         # Use `BaseException` to capture `KeyboardInterrupt` and normal `Exceptioin`.
         except BaseException as e:
-            lazyllm.LOG.warning(f"Huggingface: {e}")
+            lazyllm.LOG.warning(f"Download encountered an error: {e}")
             if not self.token:
                 lazyllm.LOG.warning('Token is empty, which may prevent private models from being downloaded, '
                                     'as indicated by "the model does not exist." Please set the token with the '
@@ -158,14 +180,119 @@ class ModelManager():
                 lazyllm.LOG.warning(f"{full_model_dir} removed due to exceptions.")
         return model
 
-    def _download_model_from_hf(self, model_name='', model_dir=''):
+class HubDownloader(ABC):
+
+    def __init__(self, call_back=None, token=None):
+        self._call_back = call_back
+        self._token = token
+        self._api = self._build_hub_api(self._token)
+        self.hub_model_info = None
+        self.total = None
+        self._run_polling = True
+
+    @abstractmethod
+    def _verify_hub_token(self, token):
+        pass
+
+    @abstractmethod
+    def _build_hub_api(self, token):
+        pass
+
+    @abstractmethod
+    def _verify_model_id(self, model_id):
+        pass
+
+    @abstractmethod
+    def _do_download(self, model_id, model_dir):
+        pass
+
+    @abstractmethod
+    def _get_repo_files(self, model_id):
+        pass
+
+    def _polling_progress(self, model_dir):
+        assert self.total
+        while self._run_polling:
+            n = self._get_current_files_size(model_dir)
+            n = min(n, self.total)
+            if callable(self._call_back):
+                try:
+                    self._call_back(n, self.total)
+                except Exception as e:
+                    print(f"Error in callback: {e}")
+            time.sleep(1)
+
+    def _get_current_files_size(self, model_dir):
+        total_size = 0
+        for dirpath, _, filenames in os.walk(model_dir):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                if os.path.isfile(fp):
+                    total_size += os.path.getsize(fp)
+        return total_size
+
+    def _get_files_total_size(self, hub_model_info):
+        size = 0
+        for item in hub_model_info:
+            size += item['Size']
+        return size
+
+    def download(self, model_id, model_dir):
+        self.hub_model_info = self._get_repo_files(model_id)
+        self.total = self._get_files_total_size(self.hub_model_info)
+        if self._call_back:
+            self._run_polling = True
+            polling_thread = threading.Thread(target=self._polling_progress, args=(model_dir,))
+            polling_thread.daemon = True
+            polling_thread.start()
+        downloaded_path = self._do_download(model_id, model_dir)
+        if self._call_back and polling_thread:
+            self._run_polling = False
+            polling_thread.join()
+        return downloaded_path
+
+    def verify_hub_token(self):
+        if hasattr(self, 'token_valid'):
+            return self.token_valid
+        else:
+            return self._verify_hub_token(self._token)
+
+    def verify_model_id(self, model_id):
+        return self._verify_model_id(model_id)
+
+class HuggingfaceDownloader(HubDownloader):
+
+    def _build_hub_api(self, token):
+        from huggingface_hub import HfApi
+        if token and self._verify_hub_token(token):
+            self.token_valid = True
+            return HfApi(token=token)
+        else:
+            self.token_valid = False
+            self._token = None
+            return HfApi()
+
+    def _verify_hub_token(self, token):
+        from huggingface_hub import HfApi
+        api = HfApi()
+        try:
+            api.whoami(token)
+            return True
+        except Exception as e:
+            lazyllm.LOG.warning('Verify failed: ', e)
+            return False
+
+    def _verify_model_id(self, model_id):
+        try:
+            self._api.model_info(model_id)
+            return True
+        except Exception as e:
+            lazyllm.LOG.warning('Verify failed: ', e)
+            return False
+
+    def _do_download(self, model_id, model_dir):
         from huggingface_hub import snapshot_download
         # refer to https://huggingface.co/docs/huggingface_hub/v0.23.1/en/package_reference/file_download
-        if self.token == '':
-            self.token = None
-        elif self.token.lower() == 'true':
-            self.token = True
-        # else token would be a string from the user.
         env_vars = {'https_proxy': lazyllm.config['https_proxy'] or os.environ.get("https_proxy", None),
                     'http_proxy': lazyllm.config['http_proxy'] or os.environ.get("http_proxy", None)}
         with EnvVarContextManager(env_vars):
@@ -175,20 +302,74 @@ class ModelManager():
                                     'to configure a proxy. Do not directly set the `https_proxy` and `http_proxy` '
                                     'environment variables in your environment, as doing so may disrupt model '
                                     'deployment and result in deployment failures.')
-            model_dir_result = snapshot_download(repo_id=model_name, local_dir=model_dir, token=self.token)
+            if not self._verify_model_id(model_id):
+                raise RuntimeError(f"Invalid model id:{model_id}")
+            downloaded_path = snapshot_download(repo_id=model_id, local_dir=model_dir, token=self._token)
+        lazyllm.LOG.info(f"model downloaded at {downloaded_path}")
+        return downloaded_path
 
-        lazyllm.LOG.info(f"model downloaded at {model_dir_result}")
-        return model_dir_result
+    def _get_repo_files(self, model_id):
+        assert self._api
+        orgin_info = self._api.list_repo_tree(model_id, expand=True, recursive=True)
+        hub_model_info = []
+        for item in list(orgin_info):
+            if hasattr(item, 'size'):
+                hub_model_info.append({
+                    'Path': item.path,
+                    'Size': item.size,
+                    'SHA': item.blob_id,
+                })
+        return hub_model_info
 
-    def _download_model_from_ms(self, model_name='', model_dir=''):
+class ModelscopeDownloader(HubDownloader):
+
+    def _build_hub_api(self, token):
+        from modelscope.hub.api import HubApi
+        api = HubApi()
+        if token and self._verify_hub_token(token):
+            api.login(token)
+            self.token_valid = True
+        else:
+            self._token = None
+            self.token_valid = False
+        return api
+
+    def _verify_hub_token(self, token):
+        from modelscope.hub.api import HubApi
+        api = HubApi()
+        try:
+            api.login(token)
+            return True
+        except Exception as e:
+            lazyllm.LOG.warning('Verify failed: ', e)
+            return False
+
+    def _verify_model_id(self, model_id):
+        try:
+            self._api.get_model(model_id)
+            return True
+        except Exception as e:
+            lazyllm.LOG.warning('Verify failed: ', e)
+            return False
+
+    def _do_download(self, model_id, model_dir):
         from modelscope.hub.snapshot_download import snapshot_download
         # refer to https://www.modelscope.cn/docs/models/download
+        if not self._verify_model_id(model_id):
+            raise RuntimeError(f"Invalid model id:{model_id}")
+        downloaded_path = snapshot_download(model_id=model_id, local_dir=model_dir)
+        lazyllm.LOG.info(f"Model downloaded at {downloaded_path}")
+        return downloaded_path
 
-        if (len(self.token) > 0):
-            from modelscope.hub.api import HubApi
-            api = HubApi()
-            api.login(self.token)
-        model_dir_result = snapshot_download(model_id=model_name, local_dir=model_dir)
-
-        lazyllm.LOG.info(f"Model downloaded at {model_dir_result}")
-        return model_dir_result
+    def _get_repo_files(self, model_id):
+        assert self._api
+        orgin_info = self._api.get_model_files(model_id, recursive=True)
+        hub_model_info = []
+        for item in orgin_info:
+            if item['Type'] == 'blob':
+                hub_model_info.append({
+                    'Path': item['Path'],
+                    'Size': item['Size'],
+                    'SHA': item['Sha256']
+                })
+        return hub_model_info
