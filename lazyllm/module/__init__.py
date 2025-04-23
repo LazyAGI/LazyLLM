189e84de2f6fe12c3da00bf2b6054b86b32b70d9
wangjian <wangjian2@sensetime.com>
2025-04-23 14:54:44 +0800

    Modify the default model of Doubao and first try to read the model name from the environment variable

--- lazyllm/module/__init__.py
+++ lazyllm/module/__init__.py
@@ -19,6 +19,8 @@ lazyllm.config.add("sensenova_api_key", str, "", "SENSENOVA_API_KEY")
 lazyllm.config.add("sensenova_secret_key", str, "", "SENSENOVA_SECRET_KEY")
 # doubao api key
 lazyllm.config.add("doubao_api_key", str, "", "DOUBAO_API_KEY")
+# doubao model name
+lazyllm.config.add("doubao_model_name", str, "", "DOUBAO_MODEL_NAME")
 # deepseek api key
 lazyllm.config.add("deepseek_api_key", str, "", "DEEPSEEK_API_KEY")
 # https proxy
