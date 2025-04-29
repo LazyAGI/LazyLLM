290ab3e2f301c029bb3221978ace4a4fef240f0d
zhangyongchao <zhangyongchao@sensetime.com>
2025-04-29 17:06:48 +0800

    fix metadata lost in document lazy_init

--- lazyllm/tools/rag/data_loaders.py
+++ lazyllm/tools/rag/data_loaders.py
@@ -3,6 +3,7 @@ from .doc_node import DocNode, ImageDocNode
 from .store_base import LAZY_ROOT_NAME, LAZY_IMAGE_GROUP
 from lazyllm import LOG
 from .dataReader import SimpleDirectoryReader
+from .global_metadata import RAG_DOC_PATH
 
 class DirectoryReader:
     def __init__(self, input_files: Optional[List[str]], local_readers: Optional[Dict] = None,
@@ -11,7 +12,7 @@ class DirectoryReader:
         self._local_readers = local_readers
         self._global_readers = global_readers
 
-    def load_data(self, input_files: Optional[List[str]] = None, metadates: Optional[Dict] = None) -> List[DocNode]:
+    def load_data(self, input_files: Optional[List[str]] = None, metadatas: Optional[Dict] = None) -> List[DocNode]:
         input_files = input_files or self._input_files
         file_readers = self._local_readers.copy()
         for key, func in self._global_readers.items():
@@ -26,5 +27,14 @@ class DirectoryReader:
             LOG.warning(
                 f"No nodes load from path {input_files}, please check your data path."
             )
+        map_file_meta = {}
+        if metadatas:
+            for file_path, metadata in zip(input_files, metadatas):
+                if metadata:
+                    map_file_meta[file_path] = metadata
+        for node in nodes:
+            file_path = node.global_metadata[RAG_DOC_PATH]
+            if file_path in map_file_meta:
+                node.global_metadata.update(map_file_meta[file_path])
         LOG.info("DirectoryReader loads data done!")
         return nodes
