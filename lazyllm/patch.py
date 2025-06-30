080c8066cf2f3d5a8472d61555a5c821b77ea7c9
wangzhihong <wangzhihong@sensetime.com>
2025-06-30 17:44:46 +0800

    fix proxy bug

---
+++ lazyllm/patch.py
@@ -0,0 +1,28 @@
+import requests
+from urllib.parse import urlparse
+import ipaddress
+
+def _is_ip_address_url(url: str) -> bool:
+    try:
+        hostname = urlparse(url).hostname
+        if hostname is None:
+            return False
+        ipaddress.ip_address(hostname)
+        return True
+    except ValueError:
+        return False
+
+# patch requests
+_old_request = requests.request
+
+def new_request(method, url, **kwargs):
+    if _is_ip_address_url(url):
+        try:
+            kw = kwargs.copy()
+            kw['proxies'] = kw.get('proxies', {'http': None, 'https': None})
+            return _old_request(method, url, **kw)
+        except Exception:
+            if kwargs.get('proxies'): raise
+    return _old_request(method, url, **kwargs)
+
+requests.request = new_request
