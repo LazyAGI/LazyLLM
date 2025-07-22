f5d6b4de352817d716a7cf556184062d99cb037f
dengyuang <dengyuang@163.com>
2025-07-22 19:20:54 +0800

    feat(qwen): enable online multimodal model support for multiple users

--- lazyllm/module/llms/onlineMultiModal/qwenModule.py
+++ lazyllm/module/llms/onlineMultiModal/qwenModule.py
@@ -150,7 +150,7 @@ class QwenTTSModule(QwenModule):
             "speech_rate": speech_rate,
             "volume": volume,
             "pitch": pitch,
-            "api_key": self._api_key,
             **kwargs
         }
+        if self._api_key: call_params['api_key'] = self._api_key
         return encode_query_with_filepaths(None, bytes_to_file(self._synthesizer_func(**call_params)))
