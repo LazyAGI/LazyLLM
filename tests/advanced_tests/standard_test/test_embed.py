850cd7c7969cbb7250ad2724a01d512f5ef4c1f1
Chenjiahao <chenjiahao@sensetime.com>
2025-09-16 16:47:27 +0800

    code style

--- tests/advanced_tests/standard_test/test_embed.py
+++ tests/advanced_tests/standard_test/test_embed.py
@@ -3,10 +3,10 @@ from lazyllm.tools.rag.embed_wrapper import _EmbedWrapper
 
 class TestEmbed(object):
     def test_embed_batch(self):
-        embed_model = lazyllm.TrainableModule("bge-large-zh-v1.5").start()
+        embed_model = lazyllm.TrainableModule('bge-large-zh-v1.5').start()
         embed_model = _EmbedWrapper(embed_model)
-        vec1 = embed_model("床前明月光")
-        vec2 = embed_model(["床前明月光", "疑是地上霜"])
+        vec1 = embed_model('床前明月光')
+        vec2 = embed_model(['床前明月光', '疑是地上霜'])
         assert len(vec2) == 2
         assert len(vec2[0]) == len(vec1)
         assert len(vec2[1]) == len(vec1)
@@ -14,18 +14,18 @@ class TestEmbed(object):
     def test_online_embed_batch(self):
         embed_model = lazyllm.OnlineEmbeddingModule()
         embed_model2 = lazyllm.OnlineEmbeddingModule(num_worker=4)
-        vec1 = embed_model("床前明月光")
-        vec2 = embed_model(["床前明月光", "疑是地上霜"])
+        vec1 = embed_model('床前明月光')
+        vec2 = embed_model(['床前明月光', '疑是地上霜'])
         assert len(vec2) == 2
         assert len(vec2[0]) == len(vec1)
         assert len(vec2[1]) == len(vec1)
-        vec3 = embed_model2(["床前明月光", "疑是地上霜", "床前明月光", "疑是地上霜", "床前明月光", "疑是地上霜", "床前明月光", "疑是地上霜"])
+        vec3 = embed_model2(['床前明月光', '疑是地上霜', '床前明月光', '疑是地上霜', '床前明月光', '疑是地上霜', '床前明月光', '疑是地上霜'])
         assert len(vec3) == 8
         assert len(vec3[0]) == len(vec1)
         assert len(vec3[1]) == len(vec1)
 
     def test__embed_adjust_batch(self):
-        embed_model = lazyllm.OnlineEmbeddingModule(source="qwen", embed_model_name="text-embedding-v3",
+        embed_model = lazyllm.OnlineEmbeddingModule(source='qwen', embed_model_name='text-embedding-v3',
                                                     num_worker=4, batch_size=20)
-        vec2 = embed_model(["床前明月光" for i in range(0, 20)])
+        vec2 = embed_model(['床前明月光' for i in range(0, 20)])
         assert len(vec2) == 20
--- tests/basic_tests/test_store.py
+++ tests/basic_tests/test_store.py
@@ -10,27 +10,27 @@ from lazyllm.tools.rag.global_metadata import RAG_DOC_ID, RAG_KB_ID
 data = [
     {'uid': 'uid1', 'doc_id': 'doc1', 'group': 'g1', 'content': 'test1', 'meta': {},
      'global_meta': {RAG_DOC_ID: 'doc1', RAG_KB_ID: 'kb1'},
-     'embedding': {'vec_dense': [0.1, 0.2, 0.3], 'vec_sparse': {"1563": 0.212890625, "238": 0.1768798828125}},
+     'embedding': {'vec_dense': [0.1, 0.2, 0.3], 'vec_sparse': {'1563': 0.212890625, '238': 0.1768798828125}},
      'type': 1, 'number': 0, 'kb_id': 'kb1',
      'excluded_embed_metadata_keys': ['file_size', 'file_name', 'file_type'],
      'excluded_llm_metadata_keys': ['file_size', 'file_name', 'file_type'],
-     'parent': None, 'answer': "", 'image_keys': []},
+     'parent': None, 'answer': '', 'image_keys': []},
 
     {'uid': 'uid2', 'doc_id': 'doc2', 'group': 'g2', 'content': 'test2', 'meta': {},
      'global_meta': {RAG_DOC_ID: 'doc2', RAG_KB_ID: 'kb2'},
-     'embedding': {'vec_dense': [0.3, 0.2, 0.1], 'vec_sparse': {"1563": 0.212890625, "238": 0.1768798828125}},
+     'embedding': {'vec_dense': [0.3, 0.2, 0.1], 'vec_sparse': {'1563': 0.212890625, '238': 0.1768798828125}},
      'type': 1, 'number': 0, 'kb_id': 'kb2',
      'excluded_embed_metadata_keys': ['file_size', 'file_name', 'file_type'],
      'excluded_llm_metadata_keys': ['file_size', 'file_name', 'file_type'],
-     'parent': 'p2', 'answer': "", 'image_keys': []},
+     'parent': 'p2', 'answer': '', 'image_keys': []},
 
     {'uid': 'uid3', 'doc_id': 'doc3', 'group': 'g1', 'content': 'test3', 'meta': {},
      'global_meta': {RAG_DOC_ID: 'doc3', RAG_KB_ID: 'kb3'},
-     'embedding': {'vec_dense': [0.3, 0.2, 0.1], 'vec_sparse': {"12": 0.212890625, "23": 0.1768798828125}},
+     'embedding': {'vec_dense': [0.3, 0.2, 0.1], 'vec_sparse': {'12': 0.212890625, '23': 0.1768798828125}},
      'type': 1, 'number': 0, 'kb_id': 'kb3',
      'excluded_embed_metadata_keys': ['file_size', 'file_name', 'file_type'],
      'excluded_llm_metadata_keys': ['file_size', 'file_name', 'file_type'],
-     'parent': None, 'answer': "", 'image_keys': []},
+     'parent': None, 'answer': '', 'image_keys': []},
 ]
 
 def clear_directory(directory_path):
@@ -43,15 +43,15 @@ def clear_directory(directory_path):
                 elif os.path.isdir(file_path):
                     shutil.rmtree(file_path)
             except Exception as e:
-                print(f"Failed to delete {file_path}. Reason: {e}")
+                print(f'Failed to delete {file_path}. Reason: {e}')
     else:
-        print(f"The directory {directory_path} does not exist.")
+        print(f'The directory {directory_path} does not exist.')
 
 
 class TestMapStore(unittest.TestCase):
     def setUp(self):
-        self.collections = ["col_g1", "col_g2"]
-        fd, self.store_dir = tempfile.mkstemp(suffix=".db")
+        self.collections = ['col_g1', 'col_g2']
+        fd, self.store_dir = tempfile.mkstemp(suffix='.db')
         os.close(fd)
         self.store1 = MapStore()
         self.store1.connect(collections=self.collections)
@@ -177,23 +177,23 @@ class TestChromadbStore(unittest.TestCase):
              'embedding': {'vec_dense': [0.1, 0.2, 0.3]}, 'type': 1, 'number': 0, 'kb_id': 'kb1',
              'excluded_embed_metadata_keys': ['file_size', 'file_name', 'file_type'],
              'excluded_llm_metadata_keys': ['file_size', 'file_name', 'file_type'],
-             'parent': None, 'answer': "", 'image_keys': []},
+             'parent': None, 'answer': '', 'image_keys': []},
             {'uid': 'uid2', 'doc_id': 'doc2', 'group': 'g2', 'content': 'test2', 'meta': {},
              'global_meta': {RAG_DOC_ID: 'doc2', RAG_KB_ID: 'kb2'},
              'embedding': {'vec_dense': [0.3, 0.2, 0.1]}, 'type': 1, 'number': 0, 'kb_id': 'kb2',
              'excluded_embed_metadata_keys': ['file_size', 'file_name', 'file_type'],
              'excluded_llm_metadata_keys': ['file_size', 'file_name', 'file_type'],
-             'parent': 'p2', 'answer': "", 'image_keys': []},
+             'parent': 'p2', 'answer': '', 'image_keys': []},
             {'uid': 'uid3', 'doc_id': 'doc3', 'group': 'g1', 'content': 'test3', 'meta': {},
              'global_meta': {RAG_DOC_ID: 'doc3', RAG_KB_ID: 'kb3'},
              'embedding': {'vec_dense': [0.3, 0.2, 0.1]}, 'type': 1, 'number': 0, 'kb_id': 'kb3',
              'excluded_embed_metadata_keys': ['file_size', 'file_name', 'file_type'],
              'excluded_llm_metadata_keys': ['file_size', 'file_name', 'file_type'],
-             'parent': None, 'answer': "", 'image_keys': []},
+             'parent': None, 'answer': '', 'image_keys': []},
         ]
-        self.collections = ["col_g1", "col_g2"]
-        self.embed_dims = {"vec_dense": 3}
-        self.embed_datatypes = {"vec_dense": DataType.FLOAT_VECTOR}
+        self.collections = ['col_g1', 'col_g2']
+        self.embed_dims = {'vec_dense': 3}
+        self.embed_datatypes = {'vec_dense': DataType.FLOAT_VECTOR}
         self.global_metadata_desc = BUILDIN_GLOBAL_META_DESC
         self.store_dir = tempfile.mkdtemp()
         self.store = ChromadbStore(uri=self.store_dir)
@@ -333,9 +333,9 @@ class TestChromadbStore(unittest.TestCase):
 @pytest.mark.skip_on_mac
 class TestMilvusStore(unittest.TestCase):
     def setUp(self):
-        self.collections = ["col_g1", "col_g2"]
-        self.embed_dims = {"vec_dense": 3}
-        self.embed_datatypes = {"vec_dense": DataType.FLOAT_VECTOR, "vec_sparse": DataType.SPARSE_FLOAT_VECTOR}
+        self.collections = ['col_g1', 'col_g2']
+        self.embed_dims = {'vec_dense': 3}
+        self.embed_datatypes = {'vec_dense': DataType.FLOAT_VECTOR, 'vec_sparse': DataType.SPARSE_FLOAT_VECTOR}
         self.global_metadata_desc = BUILDIN_GLOBAL_META_DESC
         self.index_kwargs = [
             {
@@ -355,9 +355,9 @@ class TestMilvusStore(unittest.TestCase):
                 }
             }
         ]
-        fd, self.store_dir = tempfile.mkstemp(suffix=".db")
+        fd, self.store_dir = tempfile.mkstemp(suffix='.db')
         os.close(fd)
-        self.uri_standalone = ""
+        self.uri_standalone = ''
         self.store = MilvusStore(uri=self.store_dir, index_kwargs=self.index_kwargs)
         self.store.connect(embed_dims=self.embed_dims, embed_datatypes=self.embed_datatypes,
                            global_metadata_desc=self.global_metadata_desc)
@@ -473,7 +473,7 @@ class TestMilvusStore(unittest.TestCase):
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[0].get('uid'))
         res = self.store.search(collection_name=self.collections[0],
-                                query_embedding={"1563": 0.212890625, "238": 0.1768798828125},
+                                query_embedding={'1563': 0.212890625, '238': 0.1768798828125},
                                 embed_key='vec_sparse', topk=1)
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[0].get('uid'))
@@ -482,7 +482,7 @@ class TestMilvusStore(unittest.TestCase):
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[2].get('uid'))
         res = self.store.search(collection_name=self.collections[0],
-                                query_embedding={"12": 0.212890625, "23": 0.1768798828125},
+                                query_embedding={'12': 0.212890625, '23': 0.1768798828125},
                                 embed_key='vec_sparse', topk=1)
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[2].get('uid'))
@@ -507,8 +507,8 @@ class TestMilvusStore(unittest.TestCase):
                                 embed_key='vec_dense', topk=1, filters={RAG_KB_ID: ['kb1']})
         self.assertEqual(len(res), 0)
 
-    @pytest.mark.skip(reason=("local test for milvus standalone, please set up a milvus standalone server"
-                              " and set the uri to the server"))
+    @pytest.mark.skip(reason=('local test for milvus standalone, please set up a milvus standalone server'
+                              ' and set the uri to the server'))
     def test_milvus_standalone(self):
         self.store1 = MilvusStore(uri=self.uri_standalone, index_kwargs=self.index_kwargs)
         self.store1.connect(embed_dims=self.embed_dims, embed_datatypes=self.embed_datatypes,
@@ -521,11 +521,11 @@ class TestMilvusStore(unittest.TestCase):
         self.assertEqual(res[0].get('uid'), data[0].get('uid'))
 
 
-@pytest.mark.skip(reason="To test open search store, please set up a open search server")
+@pytest.mark.skip(reason='To test open search store, please set up a open search server')
 class TestOpenSearchStore(unittest.TestCase):
     def setUp(self):
-        self.collections = ["col_g1", "col_g2"]
-        self.uri = ""
+        self.collections = ['col_g1', 'col_g2']
+        self.uri = ''
         self.client_kwargs = {}
         self.store = OpenSearchStore(uris=self.uri, client_kwargs=self.client_kwargs)
         self.store.connect()
@@ -628,44 +628,44 @@ class TestOpenSearchStore(unittest.TestCase):
         self.assertEqual(len(res), 2)
 
 
-@pytest.mark.skip(reason="To test sensecore store, please set up a sensecore rag-store server")
+@pytest.mark.skip(reason='To test sensecore store, please set up a sensecore rag-store server')
 class TestSenseCoreStore(unittest.TestCase):
     def setUp(self):
         # sensecore store need kb_id when get or delete
-        self.collections = ["col_block", "col_line"]
+        self.collections = ['col_block', 'col_line']
         self.data = [
             {'uid': 'uid1', 'doc_id': 'doc1', 'group': 'block', 'content': 'test1', 'meta': {},
              'global_meta': {RAG_DOC_ID: 'doc1', RAG_KB_ID: 'kb1'},
              'embedding': {'bge_m3_dense': [0.1, 0.2, 0.3],
-                           'bge_m3_sparse': {"1563": 0.212890625, "238": 0.1768798828125}},
+                           'bge_m3_sparse': {'1563': 0.212890625, '238': 0.1768798828125}},
              'type': 1, 'number': 0, 'kb_id': 'kb1',
              'excluded_embed_metadata_keys': ['file_size', 'file_name', 'file_type'],
              'excluded_llm_metadata_keys': ['file_size', 'file_name', 'file_type'],
-             'parent': None, 'answer': "", 'image_keys': []},
+             'parent': None, 'answer': '', 'image_keys': []},
 
             {'uid': 'uid2', 'doc_id': 'doc2', 'group': 'line', 'content': 'test2', 'meta': {},
              'global_meta': {RAG_DOC_ID: 'doc2', RAG_KB_ID: 'kb2'},
              'embedding': {'bge_m3_dense': [0.3, 0.2, 0.1],
-                           'bge_m3_sparse': {"1563": 0.212890625, "238": 0.1768798828125}},
+                           'bge_m3_sparse': {'1563': 0.212890625, '238': 0.1768798828125}},
              'type': 1, 'number': 0, 'kb_id': 'kb2',
              'excluded_embed_metadata_keys': ['file_size', 'file_name', 'file_type'],
              'excluded_llm_metadata_keys': ['file_size', 'file_name', 'file_type'],
-             'parent': 'uid1', 'answer': "", 'image_keys': []},
+             'parent': 'uid1', 'answer': '', 'image_keys': []},
         ]
         self.global_metadata_desc = BUILDIN_GLOBAL_META_DESC
-        self.uri = ""
+        self.uri = ''
         self.s3_config = {
-            "endpoint_url": os.getenv("RAG_S3_ENDPOINT", ""),
-            "access_key": os.getenv("RAG_S3_ACCESS_KEY", ""),
-            "secret_access_key": os.getenv("RAG_S3_SECRET_KEY", ""),
-            "bucket_name": os.getenv("RAG_S3_BUCKET", "rag-data"),
-            "use_minio": os.getenv("RAG_S3_USE_MINIO", "true").lower() == "true",
+            'endpoint_url': os.getenv('RAG_S3_ENDPOINT', ''),
+            'access_key': os.getenv('RAG_S3_ACCESS_KEY', ''),
+            'secret_access_key': os.getenv('RAG_S3_SECRET_KEY', ''),
+            'bucket_name': os.getenv('RAG_S3_BUCKET', 'rag-data'),
+            'use_minio': os.getenv('RAG_S3_USE_MINIO', 'true').lower() == 'true',
         }
         self.image_url_config = {
-            "access_key": os.getenv("RAG_IMAGE_URL_ACCESS_KEY", ""),
-            "secret_access_key": os.getenv("RAG_IMAGE_URL_SECRET_KEY", ""),
-            "endpoint_url": os.getenv("RAG_IMAGE_URL_ENDPOINT", ""),
-            "bucket_name": os.getenv("RAG_IMAGE_URL_BUCKET", "lazyjfs")
+            'access_key': os.getenv('RAG_IMAGE_URL_ACCESS_KEY', ''),
+            'secret_access_key': os.getenv('RAG_IMAGE_URL_SECRET_KEY', ''),
+            'endpoint_url': os.getenv('RAG_IMAGE_URL_ENDPOINT', ''),
+            'bucket_name': os.getenv('RAG_IMAGE_URL_BUCKET', 'lazyjfs')
         }
         self.store = SenseCoreStore(uri=self.uri, s3_config=self.s3_config, image_url_config=self.image_url_config)
         self.store.connect(global_metadata_desc=self.global_metadata_desc)
@@ -737,18 +737,18 @@ class TestSenseCoreStore(unittest.TestCase):
     def test_search(self):
         self.store.upsert(self.collections[0], [self.data[0]])
         self.store.upsert(self.collections[1], [self.data[1]])
-        res = self.store.search(collection_name=self.collections[0], query="test1",
+        res = self.store.search(collection_name=self.collections[0], query='test1',
                                 embed_key='bge_m3_dense', topk=1,
                                 filters={RAG_KB_ID: self.data[0].get('kb_id')})
         self.assertEqual(len(res), 1)
-        print(f"res: {res}")
+        print(f'res: {res}')
         self.assertEqual(res[0].get('uid'), self.data[0].get('uid'))
         res = self.store.search(collection_name=self.collections[0],
-                                query="test1", embed_key='bge_m3_sparse', topk=1,
+                                query='test1', embed_key='bge_m3_sparse', topk=1,
                                 filters={RAG_KB_ID: self.data[0].get('kb_id')})
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), self.data[0].get('uid'))
-        res = self.store.search(collection_name=self.collections[1], query="test2",
+        res = self.store.search(collection_name=self.collections[1], query='test2',
                                 embed_key='bge_m3_dense', topk=1,
                                 filters={RAG_KB_ID: self.data[1].get('kb_id')})
         self.assertEqual(len(res), 1)
@@ -759,9 +759,9 @@ class TestSenseCoreStore(unittest.TestCase):
 @pytest.mark.skip_on_mac
 class TestHybridStore(unittest.TestCase):
     def setUp(self):
-        self.collections = ["col_g1", "col_g2"]
-        self.embed_dims = {"vec_dense": 3}
-        self.embed_datatypes = {"vec_dense": DataType.FLOAT_VECTOR, "vec_sparse": DataType.SPARSE_FLOAT_VECTOR}
+        self.collections = ['col_g1', 'col_g2']
+        self.embed_dims = {'vec_dense': 3}
+        self.embed_datatypes = {'vec_dense': DataType.FLOAT_VECTOR, 'vec_sparse': DataType.SPARSE_FLOAT_VECTOR}
         self.global_metadata_desc = BUILDIN_GLOBAL_META_DESC
         self.index_kwargs = [
             {
@@ -781,7 +781,7 @@ class TestHybridStore(unittest.TestCase):
                 }
             }
         ]
-        fd, self.store_dir = tempfile.mkstemp(suffix=".db")
+        fd, self.store_dir = tempfile.mkstemp(suffix='.db')
         os.close(fd)
         self.segment_store = MapStore()
         self.vector_store = MilvusStore(uri=self.store_dir, index_kwargs=self.index_kwargs)
@@ -888,30 +888,30 @@ class TestHybridStore(unittest.TestCase):
     def test_search(self):
         self.store.upsert(self.collections[0], [data[0], data[2]])
         self.store.upsert(self.collections[1], [data[1]])
-        res = self.store.search(collection_name=self.collections[0], query="test1",
+        res = self.store.search(collection_name=self.collections[0], query='test1',
                                 query_embedding=[0.1, 0.2, 0.3], embed_key='vec_dense', topk=1)
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[0].get('uid'))
-        res = self.store.search(collection_name=self.collections[0], query="test1",
-                                query_embedding={"1563": 0.212890625, "238": 0.1768798828125},
+        res = self.store.search(collection_name=self.collections[0], query='test1',
+                                query_embedding={'1563': 0.212890625, '238': 0.1768798828125},
                                 embed_key='vec_sparse', topk=1)
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[0].get('uid'))
-        res = self.store.search(collection_name=self.collections[0], query="test3",
+        res = self.store.search(collection_name=self.collections[0], query='test3',
                                 query_embedding=[0.3, 0.2, 0.1], embed_key='vec_dense', topk=1)
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[2].get('uid'))
-        res = self.store.search(collection_name=self.collections[0], query="test3",
-                                query_embedding={"12": 0.212890625, "23": 0.1768798828125},
+        res = self.store.search(collection_name=self.collections[0], query='test3',
+                                query_embedding={'12': 0.212890625, '23': 0.1768798828125},
                                 embed_key='vec_sparse', topk=1)
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[2].get('uid'))
-        res = self.store.search(collection_name=self.collections[0], query="test3",
+        res = self.store.search(collection_name=self.collections[0], query='test3',
                                 query_embedding=[0.3, 0.2, 0.1], embed_key='vec_dense', topk=5)
         self.assertEqual(len(res), 2)
         self.assertEqual(res[0].get('uid'), data[2].get('uid'))
         self.assertEqual(res[1].get('uid'), data[0].get('uid'))
-        res = self.store.search(collection_name=self.collections[1], query="test2",
+        res = self.store.search(collection_name=self.collections[1], query='test2',
                                 query_embedding=[0.3, 0.2, 0.1], embed_key='vec_dense', topk=1)
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[1].get('uid'))
@@ -919,12 +919,12 @@ class TestHybridStore(unittest.TestCase):
     def test_search_with_filters(self):
         self.store.upsert(self.collections[0], [data[0], data[2]])
         self.store.upsert(self.collections[1], [data[1]])
-        res = self.store.search(collection_name=self.collections[0], query="test1",
+        res = self.store.search(collection_name=self.collections[0], query='test1',
                                 query_embedding=[0.1, 0.2, 0.3], embed_key='vec_dense', topk=2,
                                 filters={RAG_KB_ID: ['kb1']})
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].get('uid'), data[0].get('uid'))
-        res = self.store.search(collection_name=self.collections[1], query="test2",
+        res = self.store.search(collection_name=self.collections[1], query='test2',
                                 query_embedding=[0.1, 0.2, 0.3], embed_key='vec_dense', topk=1,
                                 filters={RAG_KB_ID: ['kb1']})
         self.assertEqual(len(res), 0)
