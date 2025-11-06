6d144afebad1919f03241facda96584f0ce3d63e
kjz <z98091@126.com>
2025-11-06 15:23:14 +0800

    raise error

--- lazyllm/module/llms/onlinemodule/chat.py
+++ lazyllm/module/llms/onlinemodule/chat.py
@@ -44,7 +44,7 @@ class OnlineChatModule(metaclass=_ChatModuleMeta):
         if model in OnlineChatModule.MODELS.keys() and source is None: source, model = model, source
         if type is None:
             type = get_model_type(model)
-            if type == ['embed', 'rerank', 'cross_modal_embed']:
+            if type in ['embed', 'rerank', 'cross_modal_embed']:
                 raise AssertionError(f"'{model}' should use OnlineEmbeddingModule")
             elif type in ['sst', 'tts', 'sd']:
                 raise AssertionError(f"'{model}' should use OnlineMultiModalModule")
