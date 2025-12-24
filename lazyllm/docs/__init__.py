cb3daeef6101a24e4c7e6d2c923e62e5cc49b6f5
wangzhihong <wangzhihong@sensetime.com>
2025-12-24 11:36:07 +0800

    skip build doc in default mode

--- lazyllm/docs/__init__.py
+++ lazyllm/docs/__init__.py
@@ -2,7 +2,7 @@ from .utils import add_doc
 from lazyllm import config
 
 config.add('init_doc', bool, False, 'INIT_DOC', description='whether to init docs')
-if add_doc.__doc__ is None or 'Add document' not in add_doc.__doc__:
+if config['init_doc'] and (add_doc.__doc__ is None or 'Add document' not in add_doc.__doc__):
     from . import common, components, configs, flow, hook, launcher, module, patch, prompt_template, tools, utils  # noqa F401
     del common, components, configs, flow, hook, launcher, module, patch, prompt_template, tools, utils
 
