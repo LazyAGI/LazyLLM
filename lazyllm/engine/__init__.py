fa648c87ced67e85a5a7e8bd08c02baf169b904a
yzh <perpendiculaa@qq.com>
2025-12-26 17:21:12 +0800

    Lazy import engine all in once.

--- lazyllm/engine/__init__.py
+++ lazyllm/engine/__init__.py
@@ -9,16 +9,13 @@ def __getattr__(name: str):
     if name not in __all__:
         raise AttributeError(f"Module 'engine' has no attribute '{name}'")
 
-    if name == 'Engine':
-        mod = import_module('.engine', package=__package__)
-        mod.set_default(LightEngine)
-    elif name == 'LightEngine':
-        mod = import_module('.lightengine', package=__package__)
-    elif name == 'NodeMetaHook':
-        mod = import_module('.node_meta_hook', package=__package__)
-
-    globals()[name] = value = getattr(mod, name)
-    return value
+    for obj_name, module_name in [
+        ('Engine', '.engine'),
+        ('LightEngine', '.lightengine'),
+        ('NodeMetaHook', '.node_meta_hook'),
+    ]:
+        globals()[obj_name] = import_module(module_name, package=__package__).__getattr__(obj_name)
+    return globals()[name]
 
 __all__ = [
     'Engine',
