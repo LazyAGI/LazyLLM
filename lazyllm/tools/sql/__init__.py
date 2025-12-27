11c0600312b8c23e47b74b4e668102ae0c5dc633
CompromisedKiwi <perpendiculaa@qq.com>
2025-12-27 12:00:17 +0800

    fix check error

--- lazyllm/tools/sql/__init__.py
+++ lazyllm/tools/sql/__init__.py
@@ -1,5 +1,7 @@
-from lazyllm.thirdparty import sqlalchemy # noqa F401
+from lazyllm.thirdparty import check_packages
+check_packages(['sqlalchemy'])
 
+# flake8: noqa: E401
 from .sql_manager import SqlManager
 from .mongodb_manager import MongoDBManager
 from .db_manager import DBManager, DBResult, DBStatus
