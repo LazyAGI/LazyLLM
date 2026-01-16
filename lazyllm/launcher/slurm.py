e92f3baeb3e444cdacc0fb432bbdfe8c10070266
wangzhihong <wangzhihong@sensetime.com>
2026-01-16 20:58:08 +0800

    fix bugs

--- lazyllm/launcher/slurm.py
+++ lazyllm/launcher/slurm.py
@@ -1,11 +1,11 @@
 import re
 import time
 import subprocess
-from queue import Queue
 from collections import defaultdict
 
 import lazyllm
 from lazyllm import final
+from lazyllm.common import RecentQueue as Queue
 from .base import LazyLLMLaunchersBase, Job, Status
 
 lazyllm.config.add('partition', str, 'your_part', 'SLURM_PART',
