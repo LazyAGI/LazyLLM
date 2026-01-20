98785eb6344128cf0e7f1c2f39c42fef9626e8c5
chenhao0205 <chenhao7@sensetime.com>
2026-01-20 13:15:13 +0800

    flake fix

--- tests/basic_tests/RAG/test_tools_common.py
+++ tests/basic_tests/RAG/test_tools_common.py
@@ -10,13 +10,13 @@ class TestToolsCommon(object):
             return [DocNode(str_id, text=str_id) for str_id in id_list]
 
         def retrieve_a():
-            nodes = build_nodes(["101", "203", "150", "198", "175"])
+            nodes = build_nodes(['101', '203', '150', '198', '175'])
             for i in range(0, len(nodes)):
                 nodes[i].similarity_score = 100 / (i + 1)
             return nodes
 
         def retrieve_b():
-            nodes = build_nodes(["198", "101", "110", "175", "250"])
+            nodes = build_nodes(['198', '101', '110', '175', '250'])
             for i in range(0, len(nodes)):
                 nodes[i].similarity_score = 1.0 / (i + 1)
             return nodes
@@ -26,11 +26,11 @@ class TestToolsCommon(object):
             ppl.rrf = RRFFusion(top_k=10)
         res = ppl()
         new_id_sorts = [ele._uid for ele in res]
-        assert new_id_sorts == ["101", "198", "175", "203", "150", "110", "250"]
+        assert new_id_sorts == ['101', '198', '175', '203', '150', '110', '250']
 
         rrf = RRFFusion(top_k=10)
         nodes1 = retrieve_a()
         nodes2 = retrieve_b()
         res = rrf(nodes1, nodes2)
         new_id_sorts = [ele._uid for ele in res]
-        assert new_id_sorts == ["101", "198", "175", "203", "150", "110", "250"]
+        assert new_id_sorts == ['101', '198', '175', '203', '150', '110', '250']
