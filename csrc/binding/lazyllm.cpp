0ae7b631edcfcefa6d137e4fced67e9f659ff8b4
yzh <perpendiculaa@qq.com>
2026-01-20 14:40:21 +0800

    Simple docnode.

--- csrc/binding/lazyllm.cpp
+++ csrc/binding/lazyllm.cpp
@@ -1,11 +1,21 @@
 #include "lazyllm.hpp"
 
+#include "docnode.h"
+
+namespace py = pybind11;
+
 PYBIND11_MODULE(lazyllm_cpp, m) {
     m.doc() = "LazyLLM CPP Module.";
+    exportDoc(m);
 
     // prevent document generation
-    pybind11::options options;
+    py::options options;
     options.disable_function_signatures();
 
-    exportDoc(m);
+    // DocNode
+    py::class_<lazyllm::DocNode>(m, "DocNode")
+        .def(py::init<>())
+        .def(py::init<const std::string&>(), py::arg("text"))
+        .def("set_text", &lazyllm::DocNode::set_text, py::arg("text"))
+        .def("get_text", &lazyllm::DocNode::get_text);
 }
---
+++ csrc/src/docnode.cpp
@@ -0,0 +1,15 @@
+#include "docnode.h"
+
+namespace lazyllm {
+
+DocNode::DocNode(const std::string& text) : _text(text) {}
+
+void DocNode::set_text(const std::string& text) {
+    _text = text;
+}
+
+const std::string& DocNode::get_text() const {
+    return _text;
+}
+
+} // namespace lazyllm
