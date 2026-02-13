2203dbb1419f7cbde3b8b409b6d210ef9a044196
sunxiaoye <sunxiaoye@pjlab.org.cn>
2026-02-13 19:32:33 +0800

    doc bug

--- lazyllm/prompt_templates/__init__.py
+++ lazyllm/prompt_templates/__init__.py
@@ -1,11 +1,11 @@
 from .base import BasePromptTemplate
 from .prompt_template import PromptTemplate
 from .few_shot_prompt_template import FewShotPromptTemplate
-from .prompt_library import ActorPrompt, DataPrompt
+from .prompt_library import LazyLLMPromptLibraryBase, ActorPrompt, DataPrompt
 from .prompts_data import basic_prompts  # noqa: F401
 
 __all__ = [
     'BasePromptTemplate', 'PromptTemplate',
     'FewShotPromptTemplate', 'ActorPrompt',
-    'DataPrompt'
+    'DataPrompt', 'LazyLLMPromptLibraryBase',
 ]
