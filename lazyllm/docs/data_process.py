a17ab4f487558c94a5dcddc9ae0a83a05365e076
ririqing0614 <hcj25@mails.tsinghua.edu.cn>
2026-02-14 11:31:30 +0800

    update data_process

--- lazyllm/docs/data_process.py
+++ lazyllm/docs/data_process.py
@@ -408,7 +408,7 @@ Args:
     **kwargs: extra args forwarded to the base operator (e.g. _max_workers, _save_data).
 """)
 
-add_example('data.operators.codegen_ops.CodeEnhancementInstructionGenerator', """\
+add_example('data.operators.codegen_ops.CodeEnhancementInstructionGenerator', r"""
 from lazyllm.tools.data.operators.codegen_ops import CodeEnhancementInstructionGenerator
 
 op = CodeEnhancementInstructionGenerator(model=model,
@@ -426,10 +426,10 @@ print(res)
 # {
 #    'messages': [...],
 #    'generated_instruction': "Write a Python function that prints 'hello'.\\n"
-#                             "\\`\\`\\`python\\n"
+#                             "```python\\n"
 #                             "def solution():\\n"
 #                             "    print('hello')\\n"
-#                             "\\`\\`\\`"
+#                             "```"
 # }
 """)
 
