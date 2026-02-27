a2da5f351fc059d180733b4f1512ec5a18d876be
unknown <sunhao3@sensetime.com>
2026-02-27 17:14:20 +0800

    fix format error

--- tests/charge_tests/Models/test_image_editing.py
+++ tests/charge_tests/Models/test_image_editing.py
@@ -68,16 +68,13 @@ class TestImageEditing:
         api_key = get_api_key(source)
         kwargs.setdefault('type', 'image_editing')
         img_edit = lazyllm.OnlineModule(source=source, api_key=api_key, **kwargs)
-        
         data_path = lazyllm.config['data_path']
         image_path = os.path.join(data_path, 'ci_data/dog.png')
-        
         interleaved_input = [
             '这是参考图片：',
             image_path,
             '在参考图片中的正中间添加"LazyLLM"这段英文,字体风格要和图片相同'
         ]
-        
         result = img_edit(interleaved_input)
         self._check_file_result(result)
         return result
@@ -94,7 +91,7 @@ class TestImageEditing:
     def test_qwen_image_editing_interleaved_multiple_images(self):
         api_key = get_api_key('qwen')
         img_edit = lazyllm.OnlineModule(source='qwen', api_key=api_key, type='image_editing')
-        
+
         data_path = lazyllm.config['data_path']
         image_path1 = os.path.join(data_path, 'ci_data/dog.png')
         image_path2 = os.path.join(data_path, 'ci_data/ji.png')
@@ -105,24 +102,21 @@ class TestImageEditing:
             image_path2,
             '请根据上述图片生成一张新图片，在正中间添加"LazyLLM"文字'
         ]
-        
+
         result = img_edit(interleaved_input)
         self._check_file_result(result)
 
     @pytest.mark.ignore_cache_on_change(BASE_PATH, get_path('qwen'))
     def test_qwen_image_editing_interleaved_conflict(self):
-        """Test that using both interleaved input and files parameter raises an error"""
         api_key = get_api_key('qwen')
         img_edit = lazyllm.OnlineModule(source='qwen', api_key=api_key, type='image_editing')
-        
         data_path = lazyllm.config['data_path']
         image_path = os.path.join(data_path, 'ci_data/dog.png')
-        
         interleaved_input = [
             '这是参考图：',
             image_path,
             '请生成图片'
         ]
-        
+
         with pytest.raises(AssertionError, match='Cannot use both interleaved input'):
             img_edit(interleaved_input, files=[image_path])
