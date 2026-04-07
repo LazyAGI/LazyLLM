97386d14222733f9dd21115455c1962ad1f62e4a
Mustafa974 <920998500@qq.com>
2026-04-07 14:45:55 +0800

    refine gemini's suggestion and add thread lock for config
    Signed-off-by: Mustafa974 <920998500@qq.com>

--- lazyllm/tracing/configs.py
+++ lazyllm/tracing/configs.py
@@ -1,3 +1,6 @@
+import threading
+
+
 DEFAULT_MODULE_TRACE_CONFIG = {
     'default': True,
     'by_name': {
@@ -10,6 +13,8 @@ DEFAULT_MODULE_TRACE_CONFIG = {
     },
 }
 
+_module_trace_config_lock = threading.RLock()
+
 _module_trace_config = {
     'default': DEFAULT_MODULE_TRACE_CONFIG['default'],
     'by_name': DEFAULT_MODULE_TRACE_CONFIG['by_name'].copy(),
@@ -21,27 +26,30 @@ def set_default_module_trace_config(config: dict) -> dict:
     if not isinstance(config, dict):
         raise TypeError(f'Module trace config must be dict, got {type(config).__name__}')
 
-    _module_trace_config['default'] = config.get('default', DEFAULT_MODULE_TRACE_CONFIG['default'])
-    _module_trace_config['by_name'] = dict(config.get('by_name', {}))
-    _module_trace_config['by_class'] = dict(config.get('by_class', {}))
+    with _module_trace_config_lock:
+        _module_trace_config['default'] = config.get('default', DEFAULT_MODULE_TRACE_CONFIG['default'])
+        _module_trace_config['by_name'] = dict(config.get('by_name', {}))
+        _module_trace_config['by_class'] = dict(config.get('by_class', {}))
     return get_default_module_trace_config()
 
 
 def get_default_module_trace_config() -> dict:
-    return {
-        'default': _module_trace_config['default'],
-        'by_name': _module_trace_config['by_name'].copy(),
-        'by_class': _module_trace_config['by_class'].copy(),
-    }
+    with _module_trace_config_lock:
+        return {
+            'default': _module_trace_config['default'],
+            'by_name': _module_trace_config['by_name'].copy(),
+            'by_class': _module_trace_config['by_class'].copy(),
+        }
 
 
 def resolve_default_module_trace(*, module_name=None, module_class=None) -> bool:
-    if module_name and module_name in _module_trace_config['by_name']:
-        return _module_trace_config['by_name'][module_name]
-    if module_class:
-        class_names = ([cls.__name__ for cls in module_class.mro()] if isinstance(module_class, type)
-                       else [str(module_class)])
-        for class_name in class_names:
-            if class_name in _module_trace_config['by_class']:
-                return _module_trace_config['by_class'][class_name]
-    return _module_trace_config['default']
+    with _module_trace_config_lock:
+        if module_name and module_name in _module_trace_config['by_name']:
+            return _module_trace_config['by_name'][module_name]
+        if module_class:
+            class_names = ([cls.__name__ for cls in module_class.mro()] if isinstance(module_class, type)
+                           else [str(module_class)])
+            for class_name in class_names:
+                if class_name in _module_trace_config['by_class']:
+                    return _module_trace_config['by_class'][class_name]
+        return _module_trace_config['default']
