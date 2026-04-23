9a7cc845f22abf808f6b13b7af22d39d83e8bff3
Mustafa974 <920998500@qq.com>
2026-04-23 19:49:52 +0800

    refine lambda
    Signed-off-by: Mustafa974 <920998500@qq.com>

--- lazyllm/flow/flow.py
+++ lazyllm/flow/flow.py
@@ -30,6 +30,31 @@ class FlowException(HandledException):
     pass
 
 
+class _BindPipelineAdapter(object):
+    '''Holds a ``Bind`` so it can be wrapped by ``_FuncWrap`` for tracing (see ``FlowBase._add``).'''
+
+    __slots__ = ('_b',)
+
+    def __init__(self, b):
+        self._b = b
+
+    def __call__(self, *args, **kw):
+        return self._b(*args, **kw)
+
+    def __repr__(self):
+        return repr(self._b)
+
+
+def _bind_wraps_plain_callable(b) -> bool:
+    '''True for ``lambda | bind(...)``-style steps; false when inner is already a flow/module.'''
+    if not isinstance(b, bind):
+        return False
+    inner = getattr(b, '_f', None)
+    if inner is None or not callable(inner):
+        return False
+    return not (hasattr(inner, '_flow_id') or hasattr(inner, '_module_id'))
+
+
 class _FuncWrap(object):
     def __init__(self, f):
         self._f = f._f if isinstance(f, _FuncWrap) else f
@@ -295,79 +320,33 @@ class LazyLLMFlowsBase(FlowBase, metaclass=LazyLLMRegisterMetaClass):
         self.for_each(filter, lambda x: x.job.wait())
         return self
 
-    def _prepare_invoke_item(self, it, __input, bind_args_source, kw):
-        if isinstance(it, bind):
-            if isinstance(self, Pipeline):
-                it._args = [self.output(a) if a in self._items else a for a in it._args]
-                it._kw = {k: self.output(v) if v in self._items else v for k, v in it._kw.items()}
-            kw['_bind_args_source'] = bind_args_source
-        hook_objs = []
-        if not isinstance(it, LazyLLMFlowsBase) and not hasattr(it, '_module_id'):
-            hook_objs = prepare_hooks(it, resolve_builtin_hooks(it), __input, **kw)
-        return hook_objs
-
-    @staticmethod
-    def _invoke_item_call(it, __input, kw):
-        if not isinstance(it, LazyLLMFlowsBase) and isinstance(__input, (package, kwargs)):
-            return it(*__input, **kw) if isinstance(__input, package) else it(**__input, **kw)
-        return it(__input, **kw)
-
-    def _invoke_error_position(self, it):
-        try:
-            return self._item_pos[self._items.index(it)]
-        except (ValueError, IndexError, TypeError, AttributeError):
-            return None
-
-    @staticmethod
-    def _invoke_error_kwargs(kw):
-        if '_bind_args_source' not in kw:
-            return kw
-        bind_args_source = kw.get('_bind_args_source') or {}
-        return bind_args_source.get('kwargs')
-
-    def _build_invoke_error_message(self, it, pos, __input, kw, error):
-        return (
-            f'Flow defined at {self._defined_pos or "Unknown position"} encountered an error:\n'
-            f'invoking `{it}`({pos or "Position not found"}) with input `{__input}` and kw `{kw}` failed. '
-            + 'Details: `{type}: {value}`'.format(
-                type=type(error).__name__,
-                value=str(error).replace('\n', '\\n'),
-            )
-        )
-
-    @staticmethod
-    def _run_invoke_error_hooks(hook_objs, error):
-        try:
-            run_hooks(hook_objs, 'on_error', error)
-        except Exception:
-            LOG.warning('Flow invoke on_error hook failed', exc_info=True)
-
     # bind_args: dict(input=input, args=dict(key=value))
     def invoke(self, it, __input, *, bind_args_source=None, **kw):
-        hook_objs = self._prepare_invoke_item(it, __input, bind_args_source, kw)
+        bind_step = _bind_target_for_invoke(it)
+        if bind_step is not None:
+            if isinstance(self, Pipeline):
+                bind_step._args = [self.output(a) if a in self._items else a for a in bind_step._args]
+                bind_step._kw = {k: self.output(v) if v in self._items else v for k, v in bind_step._kw.items()}
+            kw['_bind_args_source'] = bind_args_source
         try:
-            output = self._invoke_item_call(it, __input, kw)
-        except HandledException as e:
-            self._run_invoke_error_hooks(hook_objs, e)
-            raise e
+            if not isinstance(it, LazyLLMFlowsBase) and isinstance(__input, (package, kwargs)):
+                return it(*__input, **kw) if isinstance(__input, package) else it(**__input, **kw)
+            else:
+                return it(__input, **kw)
+        except HandledException as e: raise e
         except Exception as e:
-            pos = self._invoke_error_position(it)
-            invoke_kw = self._invoke_error_kwargs(kw)
-            err_msg = self._build_invoke_error_message(it, pos, __input, invoke_kw, e)
+            try:
+                pos = self._item_pos[self._items.index(it)]
+            except Exception:
+                pos = None
+            if '_bind_args_source' in kw: kw = (kw.get('_bind_args_source') or {}).pop('kwargs', None)
+            err_msg = (f'Flow defined at {self._defined_pos or "Unknown position"} encountered an error:\n'
+                       f'invoking `{it}`({pos or "Position not found"}) with input `{__input}` and kw `{kw}` failed. '
+                       + 'Details: `{type}: {value}`'.format(type=type(e).__name__, value=str(e).replace('\n', '\\n')))
             LOG.error(err_msg)
             LOG.debug(f'Error type: {type(e).__name__}, Error message: {str(e)}\n'
                       f'Traceback: {"".join(traceback.format_exception(*sys.exc_info()))}')
-            err = _change_exception_type(e, FlowException)
-            self._run_invoke_error_hooks(hook_objs, err)
-            raise err from None
-        else:
-            run_hooks(hook_objs, 'post_hook', output)
-            return output
-        finally:
-            try:
-                run_hooks(hook_objs, 'finalize')
-            except Exception:
-                LOG.warning('Flow invoke finalize hook failed', exc_info=True)
+            raise _change_exception_type(e, FlowException) from None
 
     def bind(self, *args, **kw):
         return bind(self, *args, **kw)
@@ -393,14 +372,6 @@ def save_pipeline_result(flag: bool = True):
         yield
     finally:
         _set_current_save_flag(old_flag)
-
-class _IterationTarget:
-    '''Lightweight proxy providing span naming for loop iterations.'''
-
-    def __init__(self, flow, idx):
-        self._flow_id = getattr(flow, '_flow_id', None)
-        self.__span_name__ = f'{flow.__class__.__name__}[iteration_{idx}]'
-
 
 # input -> module1 -> module2 -> ... -> moduleN -> output
 #                                               \> post-action
@@ -455,40 +426,21 @@ class Pipeline(LazyLLMFlowsBase):
         if bind_flag:
             lazyllm.LOG.debug(f'add {self.id()} to bind_args')
             locals['bind_args'][self.id()] = bind_args_source
-        is_loop = self._loop_count > 1
-        iteration_idx = -1
         for iteration_idx in range(self._loop_count):
-            iter_span = (start_span(span_kind='flow',
-                                    target=_IterationTarget(self, iteration_idx),
-                                    args=(output,), kwargs=kw)
-                         if is_loop else None)
-            stopped = False
-            try:
-                for it in self._items:
-                    output = self.invoke(it, output, bind_args_source=bind_args_source, **kw)
-                    kw.clear()
-                    bind_args_source[self.id(it)] = output
-                exp = output
-                if not self._judge_on_full_input:
-                    assert isinstance(output, tuple) and len(output) >= 2
-                    exp = output[0]
-                    output = output[1:]
-                stopped = callable(self._stop_condition) and self.invoke(self._stop_condition, exp)
-                set_span_output(iter_span, output)
-            except Exception as e:
-                set_span_error(iter_span, e)
-                raise
-            finally:
-                if iter_span:
-                    set_span_attributes(iter_span, {
-                        'lazyllm.loop.iteration': iteration_idx,
-                        'lazyllm.loop.max_count': self._loop_count,
-                        'lazyllm.loop.stopped': stopped,
-                    })
-                    finish_span(iter_span)
-            if stopped:
+            for it in self._items:
+                output = self.invoke(it, output, bind_args_source=bind_args_source, **kw)
+                kw.clear()
+                bind_args_source[self.id(it)] = output
+            exp = output
+            if not self._judge_on_full_input:
+                assert isinstance(output, tuple) and len(output) >= 2
+                exp = output[0]
+                output = output[1:]
+            if callable(self._stop_condition) and self.invoke(self._stop_condition, exp):
                 break
-        self._trace_actual_iterations = iteration_idx + 1
+        if isinstance(self, Loop):
+            # Consumed in tracing: LazyTracingHook.post_hook -> collect_trace_output_attrs (Loop)
+            self._trace_actual_iterations = iteration_idx + 1
         if bind_flag:
             lazyllm.LOG.debug(f'delete {self.id()} form bind_args')
             locals['bind_args'].pop(self.id(), None)
