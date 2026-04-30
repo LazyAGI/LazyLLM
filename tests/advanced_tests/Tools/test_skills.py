8c884487f41d8ecb107a764092349cd207ea6d83
dengyuang <dengyuang@163.com>
2026-04-30 13:00:36 +0800

    test: update skill dir parsing expectations

--- tests/advanced_tests/Tools/test_skills.py
+++ tests/advanced_tests/Tools/test_skills.py
@@ -61,12 +61,12 @@ class TestSkills(object):
         assert self._beta_name in listing
 
     def test_parse_dirs_local_expands_paths(self):
-        parsed = SkillManager._parse_dirs('~/skills', is_local=True)
+        parsed = SkillManager._parse_dirs('~/skills')
         assert parsed == [os.path.abspath(os.path.expanduser('~/skills'))]
 
-    def test_parse_dirs_non_local_preserves_paths(self):
-        parsed = SkillManager._parse_dirs('remote/skills', is_local=False)
-        assert parsed == ['remote/skills']
+    def test_parse_dirs_cloud_preserves_paths(self):
+        parsed = SkillManager._parse_dirs('s3:/remote/skills')
+        assert parsed == ['s3:/remote/skills']
 
     def test_react_agent_with_skills(self):
         llm = lazyllm.TrainableModule('Qwen2.5-32B-Instruct')
