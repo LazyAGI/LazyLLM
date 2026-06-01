971080df005c91ae8b7e0de8a1136333ca2dfdb6
Yuang-Deng <lndengyuang@gmail.com>
2026-06-01 20:49:27 +0800

    restore comment

--- lazyllm/tools/common.py
+++ lazyllm/tools/common.py
@@ -1,4 +1,9 @@
-'''Backward-compatible re-export of StreamCallHelper.'''
+'''Backward-compatible re-export of StreamCallHelper.
+
+StreamCallHelper has been moved to lazyllm.module (servermodule.py) to avoid
+circular imports between the tools and module layers. This shim keeps existing
+imports working.
+'''
 from lazyllm.module import StreamCallHelper  # noqa: F401
 
 __all__ = ['StreamCallHelper']
