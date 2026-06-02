7ca29b2d07c7118db7d385546e64fc48c3f63a89
Gxyrious <chang.liu.me@outlook.com>
2026-06-02 11:19:12 +0800

    remove read_local_storage_dir export

--- lazyllm/tracing/backends/local/__init__.py
+++ lazyllm/tracing/backends/local/__init__.py
@@ -1,9 +1,7 @@
 from .backend import LocalBackend, LocalConsumeBackend, maintain_local_traces
-from .config import read_local_storage_dir
 
 __all__ = [
     'LocalBackend',
     'LocalConsumeBackend',
     'maintain_local_traces',
-    'read_local_storage_dir',
 ]
